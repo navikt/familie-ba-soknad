2744cafb7daafeb0e03308a86bdf7348234f7293
Henrik Jackson <henrik.m.jackson@gmail.com>
2020-06-26 14:39:05 +0200

    henter dekorator eksempel 1 ved hjelp av mustache

--- server.js
+++ server.js
@@ -6,19 +6,33 @@ const path = require('path');
 const app = express();
 const setupProxy = require('./src/setupProxy');
 const getDecorator = require('./src/dekorator');
+const mustacheExpress = require("mustache-express");
+const { Console } = console;
+
+app.set("views", `${__dirname}/build`);
+app.set("view engine", "mustache");
+app.engine("html", mustacheExpress());
+
 
 setupProxy(app);
 
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
+
+// Static files
+app.use(express.static(path.join(__dirname, 'build'), { index: false }));
 
 // Nais functions
 app.get(`/internal/isAlive|isReady`, (req, res) => res.sendStatus(200));
 
 
-app.get('/', (req, res) =>
+app.get('/deko', (req, res) =>
     getDecorator()
         .then(fragments => {
-            res.render("index.html", fragments);
+            console.log("sending file");
+            //res.sendFile(path.join(__dirname + '/build/index.html'));
+            console.log("sending file OK");
+            res.render("index-dek.html", fragments);
+            console.log("render OK");
         })
         .catch(e => {
             const error = `Failed to get decorator: ${e}`;
@@ -27,10 +41,10 @@ app.get('/', (req, res) =>
         })
 );
 
-/*
+
 app.get('/', function (_req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-*/
+
 
 app.listen(9000);
