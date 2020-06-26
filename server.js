da00e77727a5588862f7820f5b4135552c23ac68
jonatan <jonatan.engstad@bekk.no>
2020-06-26 11:37:42 +0200

    legg pakker: jsdom, request, node-cache. Kopier inn dekorator-fil og bruk den i server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const setupProxy = require('./src/setupProxy');
+const getDecorator = require('./src/dekorator');
 
 setupProxy(app);
 
@@ -13,8 +14,23 @@ app.use(express.static(path.join(__dirname, 'build')));
 // Nais functions
 app.get(`/internal/isAlive|isReady`, (req, res) => res.sendStatus(200));
 
+
+app.get('/', (req, res) =>
+    getDecorator()
+        .then(fragments => {
+            res.render("index.html", fragments);
+        })
+        .catch(e => {
+            const error = `Failed to get decorator: ${e}`;
+            console.log(error);
+            res.status(500).send(error);
+        })
+);
+
+/*
 app.get('/', function (_req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+*/
 
 app.listen(9000);
