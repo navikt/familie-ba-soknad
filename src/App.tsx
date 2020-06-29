777298d67bbb0ff8f1ecc560f15c6235c4ac7ddb
Henrik Jackson <henrik.m.jackson@gmail.com>
2020-06-29 15:05:31 +0200

    Veileder lagt til i søknad + splittet opp Helse og søknad med Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.less';
 import { Systemtittel } from 'nav-frontend-typografi';
 import { AppProvider } from './context/AppContext';
 import Helse from './components/Helse/Helse';
+import VeilederSnakkebolbe from './assets/VeilederSnakkeboble';
 
 function App() {
     return (
         <AppProvider>
             <div className="App">
-                <Systemtittel>Søknad om barnetrygd</Systemtittel>
-                <Helse />
+                <Router>
+                    <Switch>
+                        <Route
+                            exact={true}
+                            path={'/'}
+                            render={() => {
+                                return (
+                                    <>
+                                        <Systemtittel>Søknad om barnetrygd</Systemtittel>
+                                        <VeilederSnakkebolbe></VeilederSnakkebolbe>
+                                    </>
+                                );
+                            }}
+                        />
+                        <Route exact={true} path={'/helse'} component={Helse} />
+                    </Switch>
+                </Router>
             </div>
         </AppProvider>
     );
