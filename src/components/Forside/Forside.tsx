b3e0922ed17eefda79d80738aa95237b1fe483e3
eirikdahlen <eirik.dahlen@nav.no>
2020-06-30 12:35:56 +0200

    legg til knapp for innsending

--- src/components/Forside/Forside.tsx
+++ src/components/Forside/Forside.tsx
@@ -1,12 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Element, Sidetittel, Normaltekst } from 'nav-frontend-typografi';
 import { BekreftCheckboksPanel } from 'nav-frontend-skjema';
+import KnappBase from 'nav-frontend-knapper';
 import VeilederSnakkeboble from '../../assets/VeilederSnakkeboble';
 import Panel from 'nav-frontend-paneler';
 
 const Forside: React.FC = () => {
     const LOREM =
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe sed consectetur maiores veniam voluptas obcaecati, cumque hic laboriosamcorporis vel temporibus optio blanditiis. Dolorum autem exercitationem nulla, sed porro eum!x';
+    const LOREM_list: String[] = [
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+        'Saepe sed consectetur maiores veniam voluptas obcaecati, cumque hic laboriosamcorporis vel temporibus optio blanditiis.',
+        'Dolorum autem exercitationem nulla, sed porro eum!x',
+    ];
 
     const [bekreftet, settBekreftet] = useState<boolean>(false);
 
@@ -23,17 +29,32 @@ const Forside: React.FC = () => {
                     </div>
                     <Sidetittel>Søknad om barnetrygd tittel</Sidetittel>
                     <Normaltekst className={'forside__innhold'}>{LOREM}</Normaltekst>
-                    <div className={'seksjon'}>
-                        <Element className="disclaimer-tittel">Dette er en disclaimer</Element>
-                        <Normaltekst>{LOREM}</Normaltekst>
-                    </div>
+
+                    {LOREM_list &&
+                        LOREM_list.map((tekst: String, index: number) => {
+                            return (
+                                <div className={'seksjon'} key={index}>
+                                    <Element className="disclaimer-tittel">
+                                        Dette er en disclaimer
+                                    </Element>
+                                    <Normaltekst>{tekst}</Normaltekst>
+                                </div>
+                            );
+                        })}
+
                     <BekreftCheckboksPanel
-                        onChange={e => handleOnChange()}
+                        onChange={() => handleOnChange()}
                         label={'Ja, jeg samtykker.'}
                         checked={bekreftet}
                     >
                         {LOREM}
                     </BekreftCheckboksPanel>
+
+                    {bekreftet ? (
+                        <KnappBase onClick={() => {}}>
+                            <h3>Start søknaden</h3>
+                        </KnappBase>
+                    ) : null}
                 </Panel>
             </div>
         </div>
