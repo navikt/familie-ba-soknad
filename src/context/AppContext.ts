639d60e6c9b41e3ce261b81349cb67dbaf4a404f
Henning Håkonsen <henninghaakonsen@me.com>
2020-07-01 16:56:29 +0200

    Tar i bruk ressurs fra familie-felles-frontend. Legger state om helse i helsekomponenten.

--- src/context/AppContext.ts
+++ src/context/AppContext.ts
@@ -2,59 +2,9 @@ import createUseContext from 'constate';
 
 import { preferredAxios, loggFeil, håndterApiRessurs } from './axios';
 import { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
-import {
-    Ressurs,
-    ApiRessurs,
-    byggTomRessurs,
-    byggFeiletRessurs,
-    byggHenterRessurs,
-} from '../typer/ressurs';
-import { useState, useEffect } from 'react';
+import { Ressurs, ApiRessurs } from '@navikt/familie-typer';
 
 const [AppProvider, useApp] = createUseContext(() => {
-    const [helseApi, settHelseApi] = useState(byggTomRessurs<string>());
-    const [helseMottak, settHelseMottak] = useState(byggTomRessurs<string>());
-    const [helsePdl, settHelsePdl] = useState(byggTomRessurs<string>());
-
-    useEffect(() => {
-        settHelseApi(byggHenterRessurs());
-        settHelseMottak(byggHenterRessurs());
-        settHelsePdl(byggHenterRessurs());
-
-        axiosRequest<string, void>({
-            url: '/api/helse/soknad-api',
-            method: 'GET',
-        })
-            .then(ressurs => {
-                settHelseApi(ressurs);
-            })
-            .catch(() => {
-                settHelseApi(byggFeiletRessurs('Helse mot backend feilet'));
-            });
-
-        axiosRequest<string, void>({
-            url: '/api/helse/mottak',
-            method: 'GET',
-        })
-            .then(ressurs => {
-                settHelseMottak(ressurs);
-            })
-            .catch(() => {
-                settHelseMottak(byggFeiletRessurs('Helse mot mottak feilet'));
-            });
-
-        axiosRequest<string, void>({
-            url: '/api/helse/pdl',
-            method: 'GET',
-        })
-            .then(ressurs => {
-                settHelsePdl(ressurs);
-            })
-            .catch(() => {
-                settHelsePdl(byggFeiletRessurs('Helse mot pdl feilet'));
-            });
-    }, []);
-
     const axiosRequest = async <T, D>(
         config: AxiosRequestConfig & { data?: D; påvirkerSystemLaster?: boolean }
     ): Promise<Ressurs<T>> => {
@@ -75,9 +25,6 @@ const [AppProvider, useApp] = createUseContext(() => {
 
     return {
         axiosRequest,
-        helseApi,
-        helseMottak,
-        helsePdl,
     };
 });
 
