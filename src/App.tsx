f8d72971d00724b28a6e020d7435d1e2e0ecb3f0
Eirik Dahlen <37441744+eirikdahlen@users.noreply.github.com>
2020-07-02 14:13:09 +0200

    Forside veileder (#8)
    * Veileder lagt til i søknad + splittet opp Helse og søknad med Router
    
    * Forside komponent for søknadssiden
    
    * styling + seksjon med disclaimer checkbox
    
    * funksjonalitet til bekreftelses-boksen med React Hooks
    
    * BekreftCheckboks cleanup
    
    * legg til knapp for innsending
    
    * style komponenter
    
    * refaktorer kode
    
    * hardkodet inn tekst og lenker fra figma-skisser
    
    * hardkodet inn mock-tekst og opprydding. mangler styling
    
    * style etter designskisse
    
    * styling for tekst i bekreftelse og knapp
    
    * stil etter design
    
    * riktig tekst, fjernet unødvendig css og korregerte feil css
    
    * legg til felleskomponent
    
    * style infoblokk + fjerne gammel css
    
    * refaktorer kode
    
    Co-authored-by: Henrik Jackson <henrik.m.jackson@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.less';
-import { Systemtittel } from 'nav-frontend-typografi';
 import { AppProvider } from './context/AppContext';
 import Helse from './components/Helse/Helse';
+import Forside from './components/Forside/Forside';
 
 function App() {
     return (
         <AppProvider>
             <div className="App">
-                <Systemtittel>Søknad om barnetrygd</Systemtittel>
-                <Helse />
+                <Router>
+                    <Switch>
+                        <Route exact={true} path={'/'} component={Forside} />
+                        <Route exact={true} path={'/helse'} component={Helse} />
+                    </Switch>
+                </Router>
             </div>
         </AppProvider>
     );
---
+++ src/components/Felleskomponenter/Informasjonsbolk/Informasjonsbolk.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Informasjonsbolk.less';
+
+export interface IInformasjonsbolkProps {
+    tittel: React.ReactNode;
+}
+
+const Informasjonsbolk: React.FC<IInformasjonsbolkProps> = ({ tittel, children }) => {
+    return (
+        <div className={'informasjonsbolk'}>
+            {tittel}
+            <div className={'informasjonsbolk__children'}>{children}</div>
+        </div>
+    );
+};
+
+export default Informasjonsbolk;
