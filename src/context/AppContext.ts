e8bc0a8e7f086651874a07e1c713c6715f047397
idaame <ida.enholm@me.com>
2020-07-07 09:09:43 +0200

    legge til liste over ressurser som laster

--- src/context/AppContext.ts
+++ src/context/AppContext.ts
@@ -14,6 +14,7 @@ import { IPerson } from '../typer/person';
 
 const [AppProvider, useApp] = createUseContext(() => {
     const [sluttbruker, settSluttbruker] = useState(byggTomRessurs<IPerson>());
+    const [ressurserSomLaster, settRessurserSomLaster] = useState<string[]>([]);
 
     console.log(sluttbruker);
 
@@ -36,24 +37,43 @@ const [AppProvider, useApp] = createUseContext(() => {
     const axiosRequest = async <T, D>(
         config: AxiosRequestConfig & { data?: D; påvirkerSystemLaster?: boolean }
     ): Promise<Ressurs<T>> => {
+        const ressursId = `${config.method}_${config.url}`;
+        config.påvirkerSystemLaster && settRessurserSomLaster([...ressurserSomLaster, ressursId]);
+
         return preferredAxios
             .request(config)
             .then((response: AxiosResponse<ApiRessurs<T>>) => {
                 const responsRessurs: ApiRessurs<T> = response.data;
+                config.påvirkerSystemLaster && fjernRessursSomLaster(ressursId);
 
                 return håndterApiRessurs(responsRessurs);
             })
             .catch((error: AxiosError) => {
                 loggFeil(error);
 
+                config.påvirkerSystemLaster && fjernRessursSomLaster(ressursId);
+
                 const responsRessurs: ApiRessurs<T> = error.response?.data;
                 return håndterApiRessurs(responsRessurs);
             });
     };
 
+    const fjernRessursSomLaster = (ressursId: string) => {
+        setTimeout(() => {
+            settRessurserSomLaster((prevState: string[]) => {
+                return prevState.filter((ressurs: string) => ressurs !== ressursId);
+            });
+        }, 300);
+    };
+
+    const systemetLaster = () => {
+        return ressurserSomLaster.length > 0;
+    };
+
     return {
         axiosRequest,
         sluttbruker,
+        systemetLaster,
     };
 });
 
