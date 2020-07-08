f8b9778e5123f404e23c376ddbc6041b4ed7fd0f
eirikdahlen <eirik.dahlen@nav.no>
2020-07-08 14:59:52 +0200

    WIP: sett default select-verdi når man klikker seg tilbake

--- src/components/SøknadsSteg/1-Søknadstype/Søknadstype.tsx
+++ src/components/SøknadsSteg/1-Søknadstype/Søknadstype.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Steg from '../Steg/Steg';
 import { Select } from 'nav-frontend-skjema';
 import { useApp, ESøknadstype } from '../../../context/AppContext';
@@ -6,14 +6,32 @@ import { useApp, ESøknadstype } from '../../../context/AppContext';
 const Søknadstype: React.FC = () => {
     const { søknad, settSøknad } = useApp();
 
+    const SØKNADS_SELECT_ID = 'søknadstype-select';
+    const options = document.getElementById(SØKNADS_SELECT_ID)?.children;
+
     console.log(søknad);
 
+    useEffect(() => {
+        settSelected();
+    }, []);
+
+    const settSelected = () => {
+        if (options) {
+            Array.from(options).forEach(option =>
+                option.getAttribute('value') === søknad.søknadstype
+                    ? option.setAttribute('selected', 'selected')
+                    : ''
+            );
+        }
+    };
+
     return (
-        <Steg tittel={'Velg søknadstype'}>
+        <Steg tittel={'Søknadstype'}>
             <Select
                 label="Velg type søknad"
                 bredde="l"
                 onChange={e => settSøknad({ søknadstype: e.target.value as ESøknadstype })}
+                id={SØKNADS_SELECT_ID}
             >
                 <option value={ESøknadstype.IKKE_SATT}>Velg type søknad</option>
                 <option value={ESøknadstype.ORDINÆR}>Ordinær</option>
--- src/components/SøknadsSteg/Steg2/Steg2.tsx
+++ src/components/SøknadsSteg/Steg2/Steg2.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import Steg from '../Steg/Steg';
+import { useApp } from '../../../context/AppContext';
 
 const Steg2: React.FC = () => {
+    const { søknad } = useApp();
+
+    console.log(søknad);
+
     return <Steg tittel={'Steg 2'}>Dette er Steg 2</Steg>;
 };
 
