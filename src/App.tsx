3c3deb7175fdc0876e5991bd5d2ac4646ac32d51
jonatan <jonaengs@gmail.com>
2020-07-09 13:23:12 +0200

    legg til disclaimer

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import './App.less';
 import { AppProvider } from './context/AppContext';
 import Søknad from './Søknad';
+import AlertStripe from 'nav-frontend-alertstriper';
 
 function App() {
     return (
         <AppProvider>
+            <AlertStripe type="advarsel">
+                Denne siden er under utvikling og skal ikke brukes.
+                <a href="https://nav.no">Vennligst besøk nav.no</a>
+            </AlertStripe>
             <Søknad />
         </AppProvider>
     );
