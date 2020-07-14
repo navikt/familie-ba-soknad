cb8a49ca1ec815f242ffba0de4549352f1b9c20d
jonaengs <32123499+jonaengs@users.noreply.github.com>
2020-07-14 12:29:11 +0200

    stopp konstant innsending av søknad (#30)

--- src/components/SøknadsSteg/1-Søknadstype/Søknadstype.tsx
+++ src/components/SøknadsSteg/1-Søknadstype/Søknadstype.tsx
@@ -1,22 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Steg from '../Steg/Steg';
 import { Select } from 'nav-frontend-skjema';
-import { useApp, ESøknadstype, ISøknad } from '../../../context/AppContext';
+import { useApp, ESøknadstype } from '../../../context/AppContext';
 
 const Søknadstype: React.FC = () => {
-    const { søknad, settSøknad, axiosRequest } = useApp();
+    const { søknad, settSøknad } = useApp();
     const label = 'Velg type søknad';
-
-    useEffect(() => {
-        axiosRequest<string, ISøknad>({
-            url: '/api/kontrakt',
-            method: 'POST',
-            withCredentials: true,
-            data: søknad,
-        })
-            .then(console.log)
-            .catch(console.log);
-    }, [søknad]);
 
     return (
         <Steg tittel={'Søknadstype'}>
