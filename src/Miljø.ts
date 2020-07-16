fe215ef1fc2bae8edd38fd582f22b41451217ca4
Henrik Jackson <henrik.m.jackson@gmail.com>
2020-07-16 10:28:59 +0200

    Knapp i oppsummering som sender søknad til backend

--- src/Miljø.ts
+++ src/Miljø.ts
@@ -1,19 +1,23 @@
 interface MiljøProps {
     loginService: string;
+    visInnsendingKnapp: boolean;
 }
 
 const Miljø = (): MiljøProps => {
     if (window.location.hostname.indexOf('familie-ba-soknad.dev') > -1) {
         return {
             loginService: 'https://loginservice.dev.nav.no/login?',
+            visInnsendingKnapp: true,
         };
     } else if (window.location.hostname.indexOf('familie-ba-soknad.nav') > -1) {
         return {
             loginService: 'https://loginservice.nav.no/login?',
+            visInnsendingKnapp: false,
         };
     } else {
         return {
             loginService: `http://localhost:8080/local/cookie?subject=12345678901`,
+            visInnsendingKnapp: true,
         };
     }
 };
--- src/components/SøknadsSteg/3-Oppsummering/Oppsummering.tsx
+++ src/components/SøknadsSteg/3-Oppsummering/Oppsummering.tsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Steg from '../Steg/Steg';
 import Oppsummeringsbolk from './Oppsummeringsbolk';
 import { visLabelOgSvar } from '../../../utils/visning';
 import { useApp } from '../../../context/AppContext';
 import { RouteEnum } from '../../../routing/Routes';
+import { ISøknad } from '../../../typer/søknad';
+import Miljø from '../../../Miljø';
+import { Hovedknapp } from 'nav-frontend-knapper';
+import { send } from 'process';
 
 const Oppsummering: React.FC = () => {
-    const { søknad } = useApp();
+    const { søknad, axiosRequest } = useApp();
+    const visInnsendingsKnapp = Miljø().visInnsendingKnapp;
+
+    {
+        /*useEffect(() => {
+        axiosRequest<string, ISøknad>({
+            url: '/api/kontrakt',
+            method: 'POST',
+            withCredentials: true,
+            påvirkerSystemLaster: true,
+            data: søknad,
+        })
+            .then(console.log)
+            .catch(console.log);
+    }, []);*/
+    }
+
+    function sendInnSøknad() {
+        axiosRequest<string, ISøknad>({
+            url: '/api/kontrakt',
+            method: 'POST',
+            withCredentials: true,
+            påvirkerSystemLaster: true,
+            data: søknad,
+        })
+            .then(console.log)
+            .catch(console.log);
+    }
 
     return (
         <Steg tittel={'Oppsummering'} erSpørsmålBesvart={true}>
@@ -29,6 +60,11 @@ const Oppsummering: React.FC = () => {
                         );
                     })}
             </Oppsummeringsbolk>
+            {visInnsendingsKnapp && (
+                <Hovedknapp type={'standard'} className={'sendInn'} onClick={sendInnSøknad}>
+                    Send inn søknad
+                </Hovedknapp>
+            )}
         </Steg>
     );
 };
