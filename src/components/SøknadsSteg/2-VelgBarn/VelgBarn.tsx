f877986748735ae0f506f9767f99d8be5ba2be20
idaame <ida.enholm@me.com>
2020-07-22 11:12:54 +0200

    fikse styling på barnekort når kun ett barn

--- src/components/SøknadsSteg/2-VelgBarn/VelgBarn.tsx
+++ src/components/SøknadsSteg/2-VelgBarn/VelgBarn.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import Steg from '../Steg/Steg';
 import Barnekort from './Barnekort/Barnekort';
 import { useApp } from '../../../context/AppContext';
+import classNames from 'classnames';
 
 const VelgBarn: React.FC = () => {
     const { søknad } = useApp();
 
     const erSpørsmålBesvart = søknad.barn.verdi.some(barn => barn.verdi.medISøknad.verdi);
+    const kunEttBarn = søknad.barn.verdi.length === 1;
 
     return (
         <Steg tittel={'Velg barn'} erSpørsmålBesvart={erSpørsmålBesvart}>
             Velg hvilke barn du vil inkludere i søknaden din
-            <div className="barnekort-container">
+            <div className={classNames('barnekort-container', { 'ett-barn': kunEttBarn })}>
                 {søknad.barn.verdi.map(barn => (
                     <Barnekort key={barn.verdi.ident.verdi} {...barn.verdi} />
                 ))}
