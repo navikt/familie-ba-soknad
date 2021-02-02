440597b9281adc8575a634103e8a3f88d253479a
norafutsaeter <nora.futseter@nav.no>
2021-02-02 11:21:31 +0100

    La til tester for komponenter

---
+++ src/components/SøknadsSteg/4-Oppsummering/Oppsummering.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import { AppProvider } from '../../../context/AppContext';
+import Oppsummering from './Oppsummering';
+
+jest.mock('./Oppsummeringsbolk', () => () => <div data-testid="oppsummeringsbolk" />);
+
+jest.mock('react-router-dom', () => ({
+    ...(jest.requireActual('react-router-dom') as {}),
+    useLocation: () => ({
+        pathname: 'localhost:3000/example/path',
+    }),
+}));
+
+test('Skal rendre tre informasjonsbolker', () => {
+    const { getAllByTestId } = render(
+        <AppProvider>
+            <Oppsummering />
+        </AppProvider>
+    );
+    expect(getAllByTestId(/oppsummeringsbolk/).length).toBe(3);
+});
