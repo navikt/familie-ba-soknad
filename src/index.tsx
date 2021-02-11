c724776b7f1a2826a769f8e4ca8ae4450597721f
Daphne <daphne.leebeek@nav.no>
2021-02-11 13:50:38 +0100

    Legg til react axe for UU-deteksjon (#72)

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import axe from '@axe-core/react';
 import ReactDOM from 'react-dom';
 
 import './index.less';
 import App from './App';
+
+if (process.env.NODE_ENV !== 'production') {
+    axe(React, ReactDOM, 1000);
+}
 
 ReactDOM.render(
     <React.StrictMode>
