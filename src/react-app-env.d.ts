c99605f29f2e0c6755c26e2b428e0d9241955844
Bjørn Snoen <bjorn.magne.snoen@nav.no>
2021-02-11 09:20:17 +0100

    (chore) Prune noen endringer fra eject

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -5,16 +5,6 @@ declare namespace NodeJS {
     }
 }
 
-declare module '*.avif' {
-    const src: string;
-    export default src;
-}
-
-declare module '*.bmp' {
-    const src: string;
-    export default src;
-}
-
 declare module '*.gif' {
     const src: string;
     export default src;
@@ -49,19 +39,4 @@ declare module '*.svg' {
 
     const src: string;
     export default src;
-}
-
-declare module '*.module.css' {
-    const classes: { readonly [key: string]: string };
-    export default classes;
-}
-
-declare module '*.module.scss' {
-    const classes: { readonly [key: string]: string };
-    export default classes;
-}
-
-declare module '*.module.sass' {
-    const classes: { readonly [key: string]: string };
-    export default classes;
 }
