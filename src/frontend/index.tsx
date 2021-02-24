4a6a52e39cea1852ea42fc694c0f778422c10437
norafutsaeter <nora.futseter@nav.no>
2021-02-24 15:51:45 +0100

    La til sentry for logging og endret rootDir

--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import * as Sentry from '@sentry/browser';
 import ReactDOM from 'react-dom';
 
+import { version } from './../../package.json';
 import './index.less';
 import App from './App';
+
+const environment = window.location.hostname;
+
+Sentry.init({
+    dsn: 'https://75e165345c514862b5829a724a4e8e45@sentry.gc.nav.no/71',
+    release: version,
+    environment,
+    //enabled: process.env.NODE_ENV !== 'development', - TODO legge til denne når vi går live
+});
 
 if (process.env.NODE_ENV !== 'production') {
     import('@axe-core/react').then(({ default: axe }) => {
