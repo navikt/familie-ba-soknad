039fa6526dcaf5fd0d31c21f4259678d764b974d
Daphne Leebeek <daphne.leebeek@nav.no>
2021-03-10 13:25:26 +0100

    Refaktorerering og implementering av layout forside

--- src/frontend/components/Forside/Forside.tsx
+++ src/frontend/components/Forside/Forside.tsx
@@ -4,11 +4,11 @@ import { FormattedMessage, useIntl } from 'react-intl';
 import { useHistory } from 'react-router';
 import styled from 'styled-components/macro';
 
-import { Hovedknapp } from 'nav-frontend-knapper';
+import navFarger from 'nav-frontend-core';
+import KnappBase from 'nav-frontend-knapper';
 import Lenke from 'nav-frontend-lenker';
-import Panel from 'nav-frontend-paneler';
 import { BekreftCheckboksPanel } from 'nav-frontend-skjema';
-import { Sidetittel, Systemtittel, Undertittel, Normaltekst } from 'nav-frontend-typografi';
+import { Sidetittel, Normaltekst } from 'nav-frontend-typografi';
 
 import { LocaleType, Sprakvelger } from '@navikt/familie-sprakvelger';
 import { RessursStatus } from '@navikt/familie-typer';
@@ -18,13 +18,48 @@ import { useApp } from '../../context/AppContext';
 import { StegRoutes } from '../../routing/Routes';
 import Informasjonsbolk from '../Felleskomponenter/Informasjonsbolk/Informasjonsbolk';
 
+const panelBredde = '524px';
+const tablet = '959px';
+
+const ForsideContainer = styled.div`
+    width: ${panelBredde};
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin: 2rem auto 20rem auto;
+    color: ${navFarger.navMorkGra};
+
+    && label,
+    a,
+    p {
+        font-size: 18px;
+        line-height: 26px;
+    }
+
+    @media all and (max-width: ${tablet}) {
+        width: 100%;
+        padding: 2rem;
+    }
+`;
+
+const StyledSidetittel = styled(Sidetittel)`
+    margin: 4rem 0 2.3rem 0;
+`;
+
 const StyledLenke = styled(Lenke)`
-    margin-top: 0.75rem;
+    margin-top: 1.125rem;
     display: inline-block;
 `;
 
-const VeilederContainer = styled.div`
-    display: inline-block;
+const StyledBekreftCheckboksPanel = styled(BekreftCheckboksPanel)`
+    padding: 1.5rem;
+    background-color: ${navFarger.navOransjeLighten80};
+    border: solid 1px ${navFarger.navOransje};
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+`;
+
+const StyledKnappBase = styled(KnappBase)`
+    margin: 2.3rem auto 0 auto;
 `;
 
 const Forside: React.FC = () => {
@@ -46,147 +81,104 @@ const Forside: React.FC = () => {
     }, []);
 
     return (
-        <div className={'forside'}>
-            <div className={'forside__innhold'}>
-                <Sprakvelger støttedeSprak={[LocaleType.en, LocaleType.nb]} />
-                <Panel className={'forside__innhold--panel'}>
-                    <VeilederContainer>
-                        <VeilederSnakkeboble
-                            tekst={formatMessage({ id: 'forside.veileder.hilsen' }, { navn: navn })}
-                            posisjon={'høyre'}
-                        />
-                    </VeilederContainer>
+        <ForsideContainer>
+            <VeilederSnakkeboble
+                tekst={formatMessage({ id: 'forside.veileder.hilsen' }, { navn: navn })}
+                posisjon={'høyre'}
+            />
 
-                    <Informasjonsbolk
-                        tittel={
-                            <Sidetittel>
-                                <FormattedMessage id={'forside.sidetittel'} />
-                            </Sidetittel>
-                        }
-                    >
+            <StyledSidetittel>
+                <FormattedMessage id="forside.sidetittel" />
+            </StyledSidetittel>
+
+            <Sprakvelger støttedeSprak={[LocaleType.en, LocaleType.nb]} />
+
+            <Informasjonsbolk>
+                <Normaltekst>
+                    <FormattedMessage
+                        id="forside.rett-på-barnetrygd"
+                        values={{ linjeskift: <br /> }}
+                    />
+                </Normaltekst>
+                <StyledLenke href={'https://www.nav.no'}>
+                    <FormattedMessage id="forside.barnetrygd-lenke-tekst" />
+                </StyledLenke>
+            </Informasjonsbolk>
+
+            <Informasjonsbolk tittelId="forside.oppmaning.tittel">
+                <Normaltekst>
+                    <FormattedMessage
+                        id="forside.oppmaning.brødtekst"
+                        values={{ linjeskift: <br /> }}
+                    />
+                </Normaltekst>
+            </Informasjonsbolk>
+
+            <Informasjonsbolk tittelId="forside.dokumentasjonskrav.tittel">
+                <Normaltekst>
+                    <FormattedMessage id="forside.dokumentasjonskrav.brødtekst" />
+                </Normaltekst>
+                <StyledLenke href={`https://www.nav.no`}>
+                    <FormattedMessage id="forside.dokumentasjonskrav.lenke" />
+                </StyledLenke>
+            </Informasjonsbolk>
+
+            <Informasjonsbolk tittelId="forside.infoinnhenting.tittel">
+                <Normaltekst>
+                    <FormattedMessage
+                        id="forside.infoinnhenting.brødtekst"
+                        values={{ linjeskift: <br /> }}
+                    />
+                </Normaltekst>
+                <ul>
+                    <li>
                         <Normaltekst>
                             <FormattedMessage
-                                id="forside.rett-på-barnetrygd"
-                                values={{ linjeskift: <br /> }}
+                                id="forside.infoinnhenting.personinformasjon"
+                                values={{ b: msg => <b>{msg}</b> }}
                             />
                         </Normaltekst>
-                        <StyledLenke href={'https://www.nav.no'}>
-                            <FormattedMessage id={'forside.barnetrygd-lenke-tekst'} />
-                        </StyledLenke>
-                    </Informasjonsbolk>
+                    </li>
+                </ul>
+                <Normaltekst>
+                    <FormattedMessage id="forside.infoinnhenting.tidligereinfo" />
+                </Normaltekst>
+                <StyledLenke href={`https://www.nav.no`}>
+                    <FormattedMessage id="forside.infoinnhenting.bruksinfolenke" />
+                </StyledLenke>
+            </Informasjonsbolk>
 
-                    <Informasjonsbolk
-                        tittel={
-                            <Systemtittel>
-                                <FormattedMessage id="forside.oppmaning.tittel" />
-                            </Systemtittel>
-                        }
-                    >
-                        <Normaltekst>
-                            <FormattedMessage
-                                id="forside.oppmaning.brødtekst"
-                                values={{ linjeskift: <br /> }}
-                            />
-                        </Normaltekst>
-                    </Informasjonsbolk>
+            <Informasjonsbolk tittelId="forside.sliksokerdu.tittel">
+                <Normaltekst>
+                    <FormattedMessage
+                        id="forside.sliksokerdu.brødtekst"
+                        values={{ linjeskift: <br /> }}
+                    />
+                </Normaltekst>
+            </Informasjonsbolk>
 
-                    <Informasjonsbolk
-                        tittel={
-                            <Undertittel>
-                                <FormattedMessage id="forside.dokumentasjonskrav.tittel" />
-                            </Undertittel>
-                        }
-                    >
-                        <Normaltekst>
-                            <FormattedMessage id="forside.dokumentasjonskrav.brødtekst" />
-                        </Normaltekst>
-                        <StyledLenke href={`https://www.nav.no`}>
-                            <FormattedMessage id="forside.dokumentasjonskrav.lenke" />
-                        </StyledLenke>
-                    </Informasjonsbolk>
+            <Informasjonsbolk tittelId="forside.bekreftelsesboks.tittel">
+                <StyledBekreftCheckboksPanel
+                    onChange={() => handleOnChange()}
+                    label={formatMessage(
+                        { id: 'forside.bekreftelsesboks.erklæring' },
+                        { navn: navn }
+                    )}
+                    checked={bekreftet}
+                >
+                    <Normaltekst>
+                        <FormattedMessage id="forside.dokumentasjonskrav.brødtekst" />
+                    </Normaltekst>
+                </StyledBekreftCheckboksPanel>
+            </Informasjonsbolk>
 
-                    <Informasjonsbolk
-                        tittel={
-                            <Undertittel>
-                                {' '}
-                                <FormattedMessage id="forside.infoinnhenting.tittel" />
-                            </Undertittel>
-                        }
-                    >
-                        <Normaltekst>
-                            <FormattedMessage
-                                id="forside.infoinnhenting.brødtekst"
-                                values={{ linjeskift: <br /> }}
-                            />
-                        </Normaltekst>
-                        <ul>
-                            <li>
-                                <Normaltekst>
-                                    <FormattedMessage
-                                        id="forside.infoinnhenting.personinformasjon"
-                                        values={{ b: msg => <b>{msg}</b> }}
-                                    />
-                                </Normaltekst>
-                            </li>
-                        </ul>
-                        <Normaltekst>
-                            <FormattedMessage id="forside.infoinnhenting.tidligereinfo" />
-                        </Normaltekst>
-                        <StyledLenke href={`https://www.nav.no`}>
-                            <FormattedMessage id="forside.infoinnhenting.bruksinfolenke" />
-                        </StyledLenke>
-                    </Informasjonsbolk>
-
-                    <Informasjonsbolk
-                        tittel={
-                            <Undertittel>
-                                <FormattedMessage id="forside.sliksokerdu.tittel" />
-                            </Undertittel>
-                        }
-                    >
-                        <Normaltekst>
-                            <FormattedMessage
-                                id="forside.sliksokerdu.brødtekst"
-                                values={{ linjeskift: <br /> }}
-                            />
-                        </Normaltekst>
-                    </Informasjonsbolk>
-
-                    <div className={'bekreftelse'}>
-                        <Undertittel className={'bekreftelse__undertittel'}>
-                            <FormattedMessage id="forside.bekreftelsesboks.tittel" />
-                        </Undertittel>
-
-                        <BekreftCheckboksPanel
-                            onChange={() => handleOnChange()}
-                            label={formatMessage(
-                                { id: 'forside.bekreftelsesboks.erklæring' },
-                                { navn: navn }
-                            )}
-                            checked={bekreftet}
-                        >
-                            {
-                                <div className={'bekreftelse__tekstområde'}>
-                                    <Normaltekst>
-                                        <FormattedMessage id="forside.dokumentasjonskrav.brødtekst" />
-                                    </Normaltekst>
-                                </div>
-                            }
-                        </BekreftCheckboksPanel>
-                    </div>
-
-                    <div className={'hovedknapp'}>
-                        {bekreftet ? (
-                            <Hovedknapp
-                                onClick={() => history.push(Object.values(StegRoutes)[0].path)}
-                            >
-                                Start søknaden
-                            </Hovedknapp>
-                        ) : null}
-                    </div>
-                </Panel>
-            </div>
-        </div>
+            <StyledKnappBase
+                type={bekreftet ? 'hoved' : 'standard'}
+                onClick={() => history.push(Object.values(StegRoutes)[0].path)}
+            >
+                Start søknaden
+            </StyledKnappBase>
+        </ForsideContainer>
     );
 };
 
