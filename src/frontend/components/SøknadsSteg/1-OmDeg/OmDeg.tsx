75cb8d143a5c69f0ab1f0505023e98fc8e916219
Bjørn Snoen <bjorn.magne.snoen@nav.no>
2021-03-11 16:14:25 +0100

    (feat) Resten av spørsmålene for steg 1

--- src/frontend/components/SøknadsSteg/1-OmDeg/OmDeg.tsx
+++ src/frontend/components/SøknadsSteg/1-OmDeg/OmDeg.tsx
@@ -3,15 +3,17 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import styled from 'styled-components/macro';
 
+import { guid } from 'nav-frontend-js-utils';
 import { Element } from 'nav-frontend-typografi';
 
-import { JaNeiSpørsmål } from '@navikt/familie-form-elements';
+import { ESvar, JaNeiSpørsmål } from '@navikt/familie-form-elements';
+import { Felt, ISkjema } from '@navikt/familie-skjema';
 
 import { LandDropdown } from '../../Felleskomponenter/LandDropdown/LandDropdown';
 import Steg from '../Steg/Steg';
 import { KomponentGruppe } from './layoutKomponenter';
 import { Personopplysninger } from './Personopplysninger';
-import { useOmdeg } from './useOmdeg';
+import { ESvarMedUbesvart, IStegEnFeltTyper, useOmdeg } from './useOmdeg';
 
 // TODO: Bruk konstant for mobilbredde
 const StyledLandDropdown = styled(LandDropdown)`
@@ -20,15 +22,50 @@ const StyledLandDropdown = styled(LandDropdown)`
         padding-right: 0.7rem;
     }
     text-align: left;
+    label {
+        font-size: 1rem;
+        font-weight: 600;
+    }
 `;
 
 const StyledSection = styled.section`
     text-align: left;
+    padding-bottom: 2rem;
 
     p {
         font-size: 1.125rem;
     }
 `;
+
+interface IJaNeiBolkProps {
+    skjema: ISkjema<IStegEnFeltTyper, string>;
+    felt: Felt<ESvar | ESvarMedUbesvart>;
+    spørsmålTekstId: string;
+}
+
+const JaNeiBolk: React.FC<IJaNeiBolkProps> = ({ skjema, felt, spørsmålTekstId }) => {
+    if (felt.erSynlig) {
+        return (
+            <JaNeiSpørsmål
+                {...felt.hentNavInputProps(skjema.visFeilmeldinger)}
+                name={guid()}
+                legend={
+                    <>
+                        <Element>
+                            <FormattedMessage id={spørsmålTekstId} />
+                        </Element>
+                    </>
+                }
+                labelTekstForJaNei={{
+                    ja: <FormattedMessage id={'ja'} />,
+                    nei: <FormattedMessage id={'nei'} />,
+                }}
+            />
+        );
+    } else {
+        return <></>;
+    }
+};
 
 const OmDeg: React.FC = () => {
     const { skjema, kanSendeSkjema } = useOmdeg();
@@ -38,29 +75,55 @@ const OmDeg: React.FC = () => {
                 <Personopplysninger skjema={skjema} />
             </KomponentGruppe>
             <StyledSection>
-                {skjema.felter.oppholderSegINorge.erSynlig && (
-                    <JaNeiSpørsmål
-                        {...skjema.felter.oppholderSegINorge.hentNavInputProps(
-                            skjema.visFeilmeldinger
-                        )}
-                        name={'søker.oppholderseginorge'}
-                        legend={
-                            <>
-                                <Element>
-                                    <FormattedMessage id={'omdeg.spm.oppholderSegINorge'} />
-                                </Element>
-                            </>
-                        }
-                        labelTekstForJaNei={{
-                            ja: <FormattedMessage id={'ja'} />,
-                            nei: <FormattedMessage id={'nei'} />,
-                        }}
-                    />
-                )}
+                <JaNeiBolk
+                    skjema={skjema}
+                    felt={skjema.felter.oppholderSegINorge}
+                    spørsmålTekstId={'omdeg.spm.oppholderSegINorge'}
+                />
                 {skjema.felter.oppholdsLand.erSynlig && (
                     <StyledLandDropdown
                         label={<FormattedMessage id={'omdeg.spm.landopphold'} />}
                         {...skjema.felter.oppholdsLand.hentNavInputProps(skjema.visFeilmeldinger)}
+                    />
+                )}
+            </StyledSection>
+            <StyledSection>
+                <JaNeiBolk
+                    skjema={skjema}
+                    felt={skjema.felter.værtINorgeITolvMåneder}
+                    spørsmålTekstId={'omdeg.spm.vært-i-tolv-måneder'}
+                />
+            </StyledSection>
+            <StyledSection>
+                <JaNeiBolk
+                    skjema={skjema}
+                    felt={skjema.felter.erAsylsøker}
+                    spørsmålTekstId={'omdeg.spm.erasylsøker'}
+                />
+            </StyledSection>
+            <StyledSection>
+                <JaNeiBolk
+                    skjema={skjema}
+                    felt={skjema.felter.jobberPåBåt}
+                    spørsmålTekstId={'omdeg.spm.jobberpåbåt'}
+                />
+                {skjema.felter.jobberPåBåt.verdi === ESvar.JA && (
+                    <StyledLandDropdown
+                        {...skjema.felter.arbeidsLand.hentNavInputProps(skjema.visFeilmeldinger)}
+                        label={<FormattedMessage id={'omdeg.spm.hvilket-arbeidsland'} />}
+                    />
+                )}
+            </StyledSection>
+            <StyledSection>
+                <JaNeiBolk
+                    skjema={skjema}
+                    felt={skjema.felter.mottarUtlandsPensjon}
+                    spørsmålTekstId={'omdeg.spm.mottar-du-pensjon-fra-utland'}
+                />
+                {skjema.felter.mottarUtlandsPensjon.verdi === ESvar.JA && (
+                    <StyledLandDropdown
+                        {...skjema.felter.pensjonsLand.hentNavInputProps(skjema.visFeilmeldinger)}
+                        label={<FormattedMessage id={'omdeg.spm.hvilket-pensjonsland'} />}
                     />
                 )}
             </StyledSection>
