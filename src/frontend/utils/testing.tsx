c5ac03ba2f414bf5ff6276c06efa2629dc449e2c
Daphne Leebeek <daphne.leebeek@nav.no>
2021-04-15 13:37:11 +0200

    Fiks feilende test

--- src/frontend/utils/testing.tsx
+++ src/frontend/utils/testing.tsx
@@ -14,6 +14,7 @@ export const spyOnUseApp = søknad => {
             søknad,
             settSisteUtfylteStegIndex: jest.fn(),
             sisteUtfylteStegIndex: 2,
+            erStegUtfyltFrafør: jest.fn(),
         })
     );
 };
