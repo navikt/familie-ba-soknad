dd071e24deb29966ff3904fd44fe9ac6ac610b1a
Daphne Leebeek <daphne.leebeek@nav.no>
2021-04-20 13:24:23 +0200

    Legg inn riktige tekster på forsiden, og oppdater tekstfil

--- src/frontend/components/Forside/Forside.tsx
+++ src/frontend/components/Forside/Forside.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { useIntl } from 'react-intl';
 import styled from 'styled-components/macro';
 
-import Lenke from 'nav-frontend-lenker';
 import { Sidetittel, Normaltekst } from 'nav-frontend-typografi';
 
 import { LocaleType, Sprakvelger } from '@navikt/familie-sprakvelger';
@@ -11,6 +10,7 @@ import { RessursStatus } from '@navikt/familie-typer';
 
 import VeilederSnakkeboble from '../../assets/VeilederSnakkeboble';
 import { useApp } from '../../context/AppContext';
+import EksternLenke from '../Felleskomponenter/EksternLenke/EksternLenke';
 import Informasjonsbolk from '../Felleskomponenter/Informasjonsbolk/Informasjonsbolk';
 import InnholdContainer from '../Felleskomponenter/InnholdContainer/InnholdContainer';
 import SpråkTekst from '../Felleskomponenter/SpråkTekst/SpråkTekst';
@@ -20,11 +20,6 @@ const StyledSidetittel = styled(Sidetittel)`
     && {
         margin: 4rem 0 2.3rem 0;
     }
-`;
-
-const StyledLenke = styled(Lenke)`
-    margin-top: 1.125rem;
-    display: inline-block;
 `;
 
 const Forside: React.FC = () => {
@@ -37,7 +32,7 @@ const Forside: React.FC = () => {
     return (
         <InnholdContainer>
             <VeilederSnakkeboble
-                tekst={formatMessage({ id: 'forside.veileder.hilsen' }, { navn: navn })}
+                tekst={formatMessage({ id: 'forside.veilederhilsen' }, { navn: navn })}
                 posisjon={'høyre'}
             />
 
@@ -49,63 +44,19 @@ const Forside: React.FC = () => {
 
             <Informasjonsbolk>
                 <Normaltekst>
-                    <SpråkTekst id="forside.rett-på-barnetrygd" values={{ linjeskift: <br /> }} />
+                    <SpråkTekst id="forside.info.punktliste" values={{ b: msg => <b>{msg}</b> }} />
                 </Normaltekst>
-                <StyledLenke href={'https://www.nav.no'}>
-                    <SpråkTekst id="forside.barnetrygd-lenke-tekst" />
-                </StyledLenke>
-            </Informasjonsbolk>
-
-            <Informasjonsbolk tittelId="forside.oppmaning.tittel">
-                <Normaltekst>
-                    <SpråkTekst id="forside.oppmaning.brødtekst" values={{ linjeskift: <br /> }} />
-                </Normaltekst>
-            </Informasjonsbolk>
-
-            <Informasjonsbolk tittelId="forside.dokumentasjonskrav.tittel">
-                <Normaltekst>
-                    <SpråkTekst id="forside.dokumentasjonskrav.brødtekst" />
-                </Normaltekst>
-                <StyledLenke href={`https://www.nav.no`}>
-                    <SpråkTekst id="forside.dokumentasjonskrav.lenke" />
-                </StyledLenke>
-            </Informasjonsbolk>
-
-            <Informasjonsbolk tittelId="forside.infoinnhenting.tittel">
-                <Normaltekst>
-                    <SpråkTekst
-                        id="forside.infoinnhenting.brødtekst"
-                        values={{ linjeskift: <br /> }}
-                    />
-                </Normaltekst>
-                <ul>
-                    <li>
-                        <Normaltekst>
-                            <SpråkTekst
-                                id="forside.infoinnhenting.personinformasjon"
-                                values={{ b: msg => <b>{msg}</b> }}
-                            />
-                        </Normaltekst>
-                    </li>
-                </ul>
-                <Normaltekst>
-                    <SpråkTekst id="forside.infoinnhenting.tidligereinfo" />
-                </Normaltekst>
-                <StyledLenke href={`https://www.nav.no`}>
-                    <SpråkTekst id="forside.infoinnhenting.bruksinfolenke" />
-                </StyledLenke>
-            </Informasjonsbolk>
-
-            <Informasjonsbolk tittelId="forside.sliksokerdu.tittel">
-                <Normaltekst>
-                    <SpråkTekst
-                        id="forside.sliksokerdu.brødtekst"
-                        values={{ linjeskift: <br /> }}
-                    />
-                </Normaltekst>
+                <EksternLenke lenkeSpråkId={'#'} lenkeTekstSpråkId={'forside.plikter.lenketekst'} />
             </Informasjonsbolk>
 
             <BekreftelseOgStartSoknad navn={navn ? navn : '-'} />
+
+            <Informasjonsbolk>
+                <EksternLenke
+                    lenkeTekstSpråkId={'forside.behandling-av-personopplysning.lenketekst'}
+                    lenkeSpråkId={'#'}
+                />
+            </Informasjonsbolk>
         </InnholdContainer>
     );
 };
