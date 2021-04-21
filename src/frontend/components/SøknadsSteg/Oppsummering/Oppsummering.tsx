0ae6e459a117f52316eb135b2ae285c6aeb4d075
norafutsaeter <nora.futseter@nav.no>
2021-04-21 11:19:37 +0200

    Komponent for OppsummeringsFelt

--- src/frontend/components/SøknadsSteg/Oppsummering/Oppsummering.tsx
+++ src/frontend/components/SøknadsSteg/Oppsummering/Oppsummering.tsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
+
+import { Element, Normaltekst } from 'nav-frontend-typografi';
 
 import { useApp } from '../../../context/AppContext';
+import SpråkTekst from '../../Felleskomponenter/SpråkTekst/SpråkTekst';
+import Oppsummeringsbolk from './Oppsummeringsbolk';
+
+interface IOppsummeringsFeltProps {
+    tittel: ReactNode;
+    søknadsvar: string;
+}
+
+const OppsummeringFelt: React.FC<IOppsummeringsFeltProps> = ({ tittel, søknadsvar }) => {
+    return (
+        <>
+            <Element>{tittel}</Element>
+            <Normaltekst>
+                <SpråkTekst id={søknadsvar} />
+            </Normaltekst>
+        </>
+    );
+};
 
 const Oppsummering: React.FC = () => {
     const { søknad } = useApp();
     console.log(søknad);
 
-    return <div>Her kommer det en oppsummering!</div>;
+    return (
+        <div>
+            {/*TODO Åpen by default*/}
+            {/*TODO skifte tittel til språktekst*/}
+            <Oppsummeringsbolk tittel={'1. Om deg'}>
+                <OppsummeringFelt
+                    tittel={<SpråkTekst id={'person.ident.visning'} />}
+                    søknadsvar={søknad.søker.ident}
+                />
+            </Oppsummeringsbolk>
+        </div>
+    );
 };
 
 export default Oppsummering;
