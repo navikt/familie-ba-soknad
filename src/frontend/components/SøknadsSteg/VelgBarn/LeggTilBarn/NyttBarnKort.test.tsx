46f20e6bef7a9377206a6cc1b126e505820e0009
Daphne <daphne.leebeek@nav.no>
2021-04-22 11:53:51 +0200

    Fix/navn input (#137)
    * Oppdater skjema typene til legg til barn
    
    * Lag egne felt for fornavn og etternavn i skjemaet
    
    * Legg på legend
    
    * Fiks feilende test
    
    * Fiks en liten feil i parameter

--- src/frontend/components/SøknadsSteg/VelgBarn/LeggTilBarn/NyttBarnKort.test.tsx
+++ src/frontend/components/SøknadsSteg/VelgBarn/LeggTilBarn/NyttBarnKort.test.tsx
@@ -56,15 +56,19 @@ test(`Kan legge til barn`, () => {
     const jaKnapp = getByText('felles.svaralternativ.ja');
     act(() => jaKnapp.click());
 
-    const navnLabel = getByText('hvilkebarn.leggtilbarn.barnets-navn');
+    const fornavnLabel = getByText('hvilkebarn.leggtilbarn.fornavn.spm');
+    const etternavnLabel = getByText('hvilkebarn.leggtilbarn.etternavn.spm');
     const idnrLabel = getByText('hvilkebarn.leggtilbarn.fnr.spm');
-    expect(navnLabel).toBeInTheDocument();
+    expect(fornavnLabel).toBeInTheDocument();
+    expect(etternavnLabel).toBeInTheDocument();
     expect(idnrLabel).toBeInTheDocument();
-    const navnInput = navnLabel.nextElementSibling || new Element();
+    const fornavnInput = fornavnLabel.nextElementSibling || new Element();
+    const etternavnInput = etternavnLabel.nextElementSibling || new Element();
     const idnrInput = idnrLabel.nextElementSibling || new Element();
 
     act(() => {
-        fireEvent.input(navnInput, { target: { value: 'Sirius Svaart' } });
+        fireEvent.input(fornavnInput, { target: { value: 'Sirius' } });
+        fireEvent.input(etternavnInput, { target: { value: 'Svaart' } });
         fireEvent.input(idnrInput, { target: { value: '031159123456' } });
     });
 
