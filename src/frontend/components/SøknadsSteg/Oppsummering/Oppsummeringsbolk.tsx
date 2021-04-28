e12a59f99f415ef093917c69db4d43dbce8dfd68
norakf <nora.futseter@nav.no>
2021-04-28 14:36:41 +0200

    Feature/oppsummering (#142)
    * Komponent for OppsummeringsFelt
    
    * Refaktor Steg så  Steg kan brukes i Oppsummering med kun tittel

--- src/frontend/components/SøknadsSteg/Oppsummering/Oppsummeringsbolk.tsx
+++ src/frontend/components/SøknadsSteg/Oppsummering/Oppsummeringsbolk.tsx
@@ -1,25 +1,39 @@
 import React from 'react';
 
 import { useHistory } from 'react-router';
+import styled from 'styled-components/macro';
 
 import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel';
 import { Undertittel } from 'nav-frontend-typografi';
 
 import { useRoutes, RouteEnum } from '../../../context/RoutesContext';
 import LenkeMedIkon from '../../Felleskomponenter/LenkeMedIkon/LenkeMedIkon';
+import SpråkTekst from '../../Felleskomponenter/SpråkTekst/SpråkTekst';
 
 interface Props {
     tittel: string;
     lenke?: RouteEnum;
 }
 
+const StyledOppsummeringsbolk = styled.div`
+    border-bottom: 1px solid #78706a;
+`;
+
 const Oppsummeringsbolk: React.FC<Props> = ({ children, tittel, lenke }) => {
     const history = useHistory();
     const { hentPath } = useRoutes();
 
     return (
-        <div className="oppsummeringsbolk">
-            <Ekspanderbartpanel tittel={<Undertittel>{tittel}</Undertittel>}>
+        <StyledOppsummeringsbolk>
+            <Ekspanderbartpanel
+                tittel={
+                    <Undertittel>
+                        <SpråkTekst id={tittel} />
+                    </Undertittel>
+                }
+                border={false}
+                apen={true}
+            >
                 {children}
                 {lenke && (
                     <LenkeMedIkon
@@ -32,7 +46,7 @@ const Oppsummeringsbolk: React.FC<Props> = ({ children, tittel, lenke }) => {
                     />
                 )}
             </Ekspanderbartpanel>
-        </div>
+        </StyledOppsummeringsbolk>
     );
 };
 
