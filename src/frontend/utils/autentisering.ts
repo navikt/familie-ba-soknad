6216dc3e10e4aed2e5d61e222131ccc5311b6277
Bjørn Snoen <bjorn.magne.snoen@nav.no>
2021-05-28 15:49:23 +0200

    Revert "(fix) Login redirect-url med path"
    This reverts commit 859a94701d431b5fd2393f13620350bbad4cf951.

--- src/frontend/utils/autentisering.ts
+++ src/frontend/utils/autentisering.ts
@@ -5,7 +5,7 @@ import Miljø from '../Miljø';
 
 const er401Feil = (error: AxiosError) => error && error.response && error.response.status === 401;
 const getLoginUrl = () => {
-    return `${Miljø().loginService}&redirect=${window.location.href}`;
+    return `${Miljø().loginService}&redirect=${window.location.origin}`;
 };
 
 export enum InnloggetStatus {
