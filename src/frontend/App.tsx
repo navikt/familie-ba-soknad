81fc8bf114d04d7ef7da3a3fccb1f86f2ee9de7a
Bjørn Snoen <bjorn.magne.snoen@nav.no>
2021-06-14 13:47:52 +0200

    (fix) Endre tilbake til to headere uten portaler

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,33 +1,22 @@
 import React from 'react';
 
-import { createPortal } from 'react-dom';
-
 import AlertStripe from 'nav-frontend-alertstriper';
 
 import AppContainer from './AppContainer';
 import { AppProvider } from './context/AppContext';
 import { RoutesProvider } from './context/RoutesContext';
 
-const DevAdvarsel: React.FC = () => {
-    return process.env.NODE_ENV !== 'production' ? (
-        <AlertStripe type="advarsel">
-            {`Denne siden er under utvikling. `}
-            <a href="https://www.nav.no/no/person/familie/barnetrygd-og-kontantstotte/barnetrygd">
-                Klikk her for å gå til våre sider for barnetrygd
-            </a>
-        </AlertStripe>
-    ) : null;
-};
-
-const DevAdvarselIPortal: React.FC = () => {
-    const header = document.querySelector('header');
-    return header ? createPortal(<DevAdvarsel />, header) : <DevAdvarsel />;
-};
-
 function App() {
     return (
         <AppProvider>
-            <DevAdvarselIPortal />
+            {process.env.NODE_ENV !== 'production' && (
+                <AlertStripe type="advarsel">
+                    {`Denne siden er under utvikling. `}
+                    <a href="https://www.nav.no/no/person/familie/barnetrygd-og-kontantstotte/barnetrygd">
+                        Klikk her for å gå til våre sider for barnetrygd
+                    </a>
+                </AlertStripe>
+            )}
             <RoutesProvider>
                 <AppContainer />
             </RoutesProvider>
