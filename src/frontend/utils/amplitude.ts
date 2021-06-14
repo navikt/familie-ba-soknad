af60dd966b9505683bc88841100dfc8dd3464772
Daphne Leebeek <daphne.leebeek@nav.no>
2021-06-14 15:31:37 +0200

    Initier amplitude

---
+++ src/frontend/utils/amplitude.ts
@@ -0,0 +1,26 @@
+import amplitude from 'amplitude-js';
+
+import { ESøknadstype, søknadstyper } from '../typer/søknad';
+
+const amplitudeInstance = amplitude.getInstance();
+
+amplitudeInstance.init('default', '', {
+    apiEndpoint: 'amplitude.nav.no/collect-auto',
+    saveEvents: false,
+    includeUtm: true,
+    includeReferrer: true,
+    platform: window.location.toString(),
+});
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export function logEvent(eventName: string, eventProperties: any) {
+    amplitudeInstance.logEvent(eventName, eventProperties);
+}
+
+export const logSidevisningOrdinærBarnetrygd = (side: string) => {
+    logEvent('sidevisning', {
+        side,
+        team_id: 'familie',
+        skjemanavn: søknadstyper[ESøknadstype.ORDINÆR].navn,
+    });
+};
