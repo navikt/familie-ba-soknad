e4c69fd94b3763360cb4a3c535530e70a7a7ffc4
André Wolden <wolden.andre@gmail.com>
2021-06-14 14:28:33 +0200

    wip test av blokkerTilbakeKnappModal

--- src/frontend/components/Felleskomponenter/BlokkerTilbakeKnappModal/blokkerTilbakeKnappModal.test.tsx
+++ src/frontend/components/Felleskomponenter/BlokkerTilbakeKnappModal/blokkerTilbakeKnappModal.test.tsx
@@ -1,24 +1,68 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { act, render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import { mockHistory, TestProvidereMedEkteTekster } from '../../../utils/testing';
+import { ISøknad } from '../../../typer/søknad';
+import {
+    mockHistory,
+    silenceConsoleErrors,
+    spyOnUseApp,
+    TestProvidere,
+} from '../../../utils/testing';
 import BlokkerTilbakeKnappModal from './BlokkerTilbakeKnappModal';
 
-mockHistory(['/dokumentasjon', '/kvittering']);
+jest.mock('@navikt/fnrvalidator');
+
+jest.mock('react-router-dom');
+
+const manueltRegistrert = {
+    ident: '12345',
+    navn: 'A',
+};
+const fraPdl = {
+    ident: '54321',
+    navn: 'B',
+};
 
 describe('Ingen navigering tilbake til søknad fra kvitteringssiden', () => {
     test(`Render BlokkerTilbakeKnappModal og sjekk at den virker`, () => {
-        render(
-            <TestProvidereMedEkteTekster>
-                <BlokkerTilbakeKnappModal />
-            </TestProvidereMedEkteTekster>,
-            { wrapper: BrowserRouter }
+        const history = mockHistory(['dokumentasjon', 'kvittering']);
+
+        silenceConsoleErrors();
+        const søknad = {
+            barnRegistrertManuelt: [manueltRegistrert],
+            barnInkludertISøknaden: [manueltRegistrert, fraPdl],
+            søker: { barn: [fraPdl] },
+            dokumentasjon: [],
+        };
+        const { settSøknad } = spyOnUseApp(søknad);
+
+        settSøknad.mockImplementation((nySøknad: ISøknad) => {
+            søknad.barnRegistrertManuelt = nySøknad.barnRegistrertManuelt;
+            søknad.barnInkludertISøknaden = nySøknad.barnInkludertISøknaden;
+        });
+
+        const { queryByText, getByText } = render(
+            <TestProvidere>
+                <Router>
+                    <div>
+                        <BlokkerTilbakeKnappModal />
+                    </div>
+                </Router>
+            </TestProvidere>
         );
 
-        // Er på kvittering side
-        // Trykker tilbake knappen (goBack() eller pop())
-        // Er fortsatt på kvitteringsside og kan se modalen.
+        act(() => {
+            history.pop();
+        });
+
+        console.warn(JSON.stringify(history, null, 4));
+
+        const infoTekst = queryByText(/felles.blokkerTilbakeKnapp.modal.tekst/);
+
+        getByText('baretullogtoys');
+
+        expect(infoTekst).toBeInTheDocument();
     });
 });
