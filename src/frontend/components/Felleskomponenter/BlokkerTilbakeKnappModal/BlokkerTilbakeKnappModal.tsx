4829c569474448e123034f604ac712c4fe753f76
André Wolden <wolden.andre@gmail.com>
2021-06-15 12:47:21 +0200

    setter inn tekster og fikser styling av modal

--- src/frontend/components/Felleskomponenter/BlokkerTilbakeKnappModal/BlokkerTilbakeKnappModal.tsx
+++ src/frontend/components/Felleskomponenter/BlokkerTilbakeKnappModal/BlokkerTilbakeKnappModal.tsx
@@ -1,36 +1,42 @@
 import React, { useState } from 'react';
 
+import { useIntl } from 'react-intl';
 import { Prompt } from 'react-router-dom';
 import styled from 'styled-components/macro';
 
-import { Knapp } from 'nav-frontend-knapper';
-import Lenke from 'nav-frontend-lenker';
+import { Flatknapp } from 'nav-frontend-knapper';
 import Modal from 'nav-frontend-modal';
-import { Normaltekst } from 'nav-frontend-typografi';
+import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
 
-import Informasjonsbolk from '../Informasjonsbolk/Informasjonsbolk';
+import Knappelenke from '@navikt/sif-common-core/lib/components/knappelenke/Knappelenke';
+
 import SpråkTekst from '../SpråkTekst/SpråkTekst';
 
 const StyledModal = styled(Modal)`
     max-width: 600px;
 `;
+const StyledUndertittel = styled(Undertittel)`
+    padding-bottom: 18px;
+`;
 const Wrapper = styled.div`
-    text-align: center;
+    padding: 9px;
 `;
 const Flex = styled.div`
+    padding-top: 18px;
     display: flex;
     flex-direction: row;
-    align-content: space-between;
     flex-wrap: wrap;
-    justify-content: center;
+    justify-content: flex-end;
+    align-content: space-between;
+    gap: 1rem;
 `;
-
-const StyledKnapp = styled(Knapp)`
-    margin: 0 2rem 1rem 2rem;
+const StyledFlatknapp = styled(Flatknapp)`
+    margin-right: 1rem;
 `;
 
 const BlokkerTilbakeKnappModal = () => {
     const [show, setShow] = useState(false);
+    const { formatMessage } = useIntl();
 
     const handleNavigation = () => {
         setShow(true);
@@ -46,30 +52,22 @@ const BlokkerTilbakeKnappModal = () => {
             <StyledModal
                 onRequestClose={() => setShow(false)}
                 isOpen={show}
-                contentLabel={'Hva skal tittelen være?'}
+                contentLabel={formatMessage({ id: 'felles.blokkerTilbakeKnapp.modal.tittel' })}
             >
                 <Wrapper>
-                    <Informasjonsbolk tittelId={'felles.blokkerTilbakeKnapp.modal.tittel'}>
-                        <Normaltekst>
-                            <SpråkTekst id={'felles.blokkerTilbakeKnapp.modal.tekst'} />
-                        </Normaltekst>
-                    </Informasjonsbolk>
+                    <StyledUndertittel>
+                        <SpråkTekst id={'felles.blokkerTilbakeKnapp.modal.tittel'} />
+                    </StyledUndertittel>
+                    <Normaltekst>
+                        <SpråkTekst id={'felles.blokkerTilbakeKnapp.modal.tekst'} />
+                    </Normaltekst>
                     <Flex>
-                        <Lenke
-                            href={'https://www.nav.no/familie/barnetrygd/soknad/ordinaer'}
-                            rel={'noopener noreferrer'}
-                        >
-                            <SpråkTekst id={'felles.blokkerTilbakeKnapp.modal.forsideKnapp'} />
-                        </Lenke>
-                        <Lenke
-                            href={'https://www.nav.no/person/dittnav/'}
-                            rel={'noopener noreferrer'}
-                        >
-                            <SpråkTekst id={'felles.blokkerTilbakeKnapp.modal.tilDittNavKnapp'} />
-                        </Lenke>
-                        <StyledKnapp onClick={håndterAvbryt}>
+                        <StyledFlatknapp onClick={håndterAvbryt}>
                             <SpråkTekst id={'felles.blokkerTilbakeKnapp.modal.avbrytKnapp'} />
-                        </StyledKnapp>
+                        </StyledFlatknapp>
+                        <Knappelenke href={'https://www.nav.no/person/dittnav/'}>
+                            <SpråkTekst id={'felles.blokkerTilbakeKnapp.modal.tilDittNavKnapp'} />
+                        </Knappelenke>
                     </Flex>
                 </Wrapper>
             </StyledModal>
