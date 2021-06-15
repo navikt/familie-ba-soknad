30f73c8b54783f506a179c831ab50db497022188
André Wolden <wolden.andre@gmail.com>
2021-06-15 16:49:13 +0200

    bytter fra px til rem og endrer knapp til lenke

--- src/frontend/components/Felleskomponenter/BlokkerTilbakeKnappModal/BlokkerTilbakeKnappModal.tsx
+++ src/frontend/components/Felleskomponenter/BlokkerTilbakeKnappModal/BlokkerTilbakeKnappModal.tsx
@@ -4,33 +4,33 @@ import { useIntl } from 'react-intl';
 import { Prompt } from 'react-router-dom';
 import styled from 'styled-components/macro';
 
-import { Flatknapp } from 'nav-frontend-knapper';
+import KnappBase from 'nav-frontend-knapper';
+import Lenke from 'nav-frontend-lenker';
 import Modal from 'nav-frontend-modal';
 import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
-
-import Knappelenke from '@navikt/sif-common-core/lib/components/knappelenke/Knappelenke';
 
 import SpråkTekst from '../SpråkTekst/SpråkTekst';
 
 const StyledModal = styled(Modal)`
-    max-width: 600px;
+    max-width: 30rem;
 `;
 const StyledUndertittel = styled(Undertittel)`
-    padding-bottom: 18px;
+    padding-bottom: 1rem;
 `;
 const Wrapper = styled.div`
-    padding: 9px;
+    padding: 0.5rem;
 `;
 const Flex = styled.div`
-    padding-top: 18px;
+    padding-top: 1rem;
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
     justify-content: flex-end;
     align-content: space-between;
+    align-items: center;
     gap: 1rem;
 `;
-const StyledFlatknapp = styled(Flatknapp)`
+const StyledLenke = styled(Lenke)`
     margin-right: 1rem;
 `;
 
@@ -62,12 +62,12 @@ const BlokkerTilbakeKnappModal = () => {
                         <SpråkTekst id={'felles.blokkerTilbakeKnapp.modal.tekst'} />
                     </Normaltekst>
                     <Flex>
-                        <StyledFlatknapp onClick={håndterAvbryt}>
-                            <SpråkTekst id={'felles.blokkerTilbakeKnapp.modal.avbrytKnapp'} />
-                        </StyledFlatknapp>
-                        <Knappelenke href={'https://www.nav.no/person/dittnav/'}>
+                        <StyledLenke href={'https://www.nav.no/person/dittnav/'}>
                             <SpråkTekst id={'felles.blokkerTilbakeKnapp.modal.tilDittNavKnapp'} />
-                        </Knappelenke>
+                        </StyledLenke>
+                        <KnappBase onClick={håndterAvbryt}>
+                            <SpråkTekst id={'felles.blokkerTilbakeKnapp.modal.avbrytKnapp'} />
+                        </KnappBase>
                     </Flex>
                 </Wrapper>
             </StyledModal>
