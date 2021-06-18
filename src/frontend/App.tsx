3bd937680e646ef937d2d9d59b650dea5731836d
André Wolden <wolden.andre@gmail.com>
2021-06-18 20:14:43 +0200

    fjerner test knapp

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import AlertStripe from 'nav-frontend-alertstriper';
 
@@ -7,23 +7,8 @@ import { AppProvider } from './context/AppContext';
 import { RoutesProvider } from './context/RoutesContext';
 
 function App() {
-    const [go, setGo] = useState(false);
-
-    useEffect(() => {
-        if (go) {
-            const a = {};
-            console.warn('Lager feil');
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            console.warn(a.b.c);
-        }
-    });
-
     return (
         <AppProvider>
-            <button onClick={() => setGo(true)}>
-                Få en feil i Sentry med tags: scope = "familie-ba-soknad"
-            </button>
             {process.env.NODE_ENV !== 'production' && (
                 <AlertStripe type="advarsel">
                     {`Denne siden er under utvikling. `}
