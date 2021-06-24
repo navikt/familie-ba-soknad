930846d9c329a6a3d665ff2276ce747ad6cd5247
Bjørn Snoen <bjorn.magne.snoen@nav.no>
2021-06-24 15:14:16 +0200

    (feat) Språkstøtte i disabledApp.

--- src/frontend/disabled.tsx
+++ src/frontend/disabled.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom';
-import { IntlProvider } from 'react-intl';
 
+import { LocaleType, SprakProvider } from '@navikt/familie-sprakvelger';
+
+import * as bokmål from './assets/lang/nb.json';
+import * as nynorsk from './assets/lang/nn.json';
 import { DisabledApp } from './components/Disabled/DisabledApp';
 import { GlobalStyle } from './Theme';
 
 ReactDOM.render(
     <React.StrictMode>
         <GlobalStyle />
-        <IntlProvider locale={'nb'}>
+        <SprakProvider
+            tekster={{ nb: { ...bokmål }, nn: { ...nynorsk } }}
+            defaultLocale={LocaleType.nb}
+        >
             <DisabledApp />
-        </IntlProvider>
+        </SprakProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
