d87e5e2d2b3645a67ddd3f86adda47ff92ac0b6d
Daphne Leebeek <daphne.leebeek@nav.no>
2021-07-02 09:32:37 +0200

    Fjern engelskmelding fra forsiden

--- src/frontend/components/SøknadsSteg/Forside/Forside.tsx
+++ src/frontend/components/SøknadsSteg/Forside/Forside.tsx
@@ -3,14 +3,12 @@ import React, { useEffect } from 'react';
 import { useIntl } from 'react-intl';
 import styled from 'styled-components/macro';
 
-import Lenke from 'nav-frontend-lenker';
 import { Sidetittel } from 'nav-frontend-typografi';
 
 import { LocaleType, Sprakvelger } from '@navikt/familie-sprakvelger';
 import { RessursStatus } from '@navikt/familie-typer';
 
 import VeilederSnakkeboble from '../../../assets/VeilederSnakkeboble';
-import AlertStripe from '../../../components/Felleskomponenter/AlertStripe/AlertStripe';
 import { useApp } from '../../../context/AppContext';
 import { RouteEnum } from '../../../context/RoutesContext';
 import Miljø from '../../../Miljø';
@@ -25,13 +23,6 @@ import FortsettPåSøknad from './FortsettPåSøknad';
 const StyledSidetittel = styled(Sidetittel)`
     && {
         margin: 4rem 0 2.3rem 0;
-    }
-`;
-
-const AlertStripeWrapper = styled.div`
-    margin-top: 2rem;
-    a {
-        display: block;
     }
 `;
 
@@ -65,19 +56,6 @@ const Forside: React.FC = () => {
             </StyledSidetittel>
 
             <StyledSpråkvelger støttedeSprak={[LocaleType.nn, LocaleType.nb, LocaleType.en]} />
-            <AlertStripeWrapper>
-                <AlertStripe type={'info'} form={'default'}>
-                    This application is currently only available in Norwegian. If you need to apply
-                    in English you have to use the PDF/paper applicationform.
-                    <Lenke
-                        href={'/soknader/en/person/familie/barnetrygd'}
-                        target={'_blank'}
-                        rel="noopener noreferrer"
-                    >
-                        Use PDF/paper form.
-                    </Lenke>
-                </AlertStripe>
-            </AlertStripeWrapper>
             <Informasjonsbolk>
                 <SpråkTekst id="forside.info.punktliste" values={{ b: msg => <b>{msg}</b> }} />
                 <EksternLenke
