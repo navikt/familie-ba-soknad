e867d6652a65f0ff012de15d0ce657b674f378b9
Bjørn Snoen <bjorn.magne.snoen@nav.no>
2021-08-03 14:06:32 +0200

    (feat) Språkstøtte i disabledApp.

--- src/frontend/components/Disabled/DisabledApp.tsx
+++ src/frontend/components/Disabled/DisabledApp.tsx
@@ -1,23 +1,41 @@
 import React from 'react';
 
+import styled from 'styled-components/macro';
+
 import Lenke from 'nav-frontend-lenker';
 import { Normaltekst, Sidetittel } from 'nav-frontend-typografi';
+
+import { LocaleType, Sprakvelger } from '@navikt/familie-sprakvelger';
 
 import VeilederSnakkeboble from '../../assets/VeilederSnakkeboble';
 import InnholdContainer from '../Felleskomponenter/InnholdContainer/InnholdContainer';
 import SpråkTekst from '../Felleskomponenter/SpråkTekst/SpråkTekst';
 
+const StyledSpråkvelger = styled(Sprakvelger)`
+    margin: auto auto 2rem auto;
+`;
+
+const StyledSidetittel = styled(Sidetittel)`
+    && {
+        margin: 4rem 0 2.3rem 0;
+    }
+`;
+
 export const DisabledApp: React.FC = () => {
     return (
         <main>
             <InnholdContainer>
+                {
+                    // TODO: Dekoratøren språk-handling fra PR: #265
+                }
                 <VeilederSnakkeboble
                     tekst={<SpråkTekst id={'vedlikehold.veilederhilsen'} />}
                     posisjon={'høyre'}
                 />
-                <Sidetittel>
+                <StyledSidetittel>
                     <SpråkTekst id={'vedlikehold.sidetittel'} />
-                </Sidetittel>
+                </StyledSidetittel>
+                <StyledSpråkvelger støttedeSprak={[LocaleType.nb, LocaleType.nn, LocaleType.en]} />
                 <Normaltekst>
                     <SpråkTekst id={'vedlikehold.brødtekst'} />
                 </Normaltekst>
