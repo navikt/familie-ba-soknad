a753261876dca4bb80cf04b02c3d1126219c8a62
André Wolden <wolden.andre@gmail.com>
2021-08-13 13:10:18 +0200

    modellVersjon 8

--- src/frontend/Miljø.ts
+++ src/frontend/Miljø.ts
@@ -6,7 +6,7 @@ interface MiljøProps {
     modellVersjon: number;
     dokumentUrl: string;
 }
-const modellVersjon = 9;
+const modellVersjon = 8;
 
 export const basePath = process.env.BASE_PATH ?? '/';
 
