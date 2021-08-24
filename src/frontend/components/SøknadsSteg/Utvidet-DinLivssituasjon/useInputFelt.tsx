8f9e97a4294fb854a4321f126354b0b603d74352
Daphne Leebeek <daphne.leebeek@nav.no>
2021-08-24 10:49:07 +0200

    Fjern unødvendig hook

--- src/frontend/components/SøknadsSteg/Utvidet-DinLivssituasjon/useInputFelt.tsx
+++ src/frontend/components/SøknadsSteg/Utvidet-DinLivssituasjon/useInputFelt.tsx
@@ -2,16 +2,15 @@ import React from 'react';
 
 import { feil, FeltState, ok, useFelt } from '@navikt/familie-skjema';
 
-import useFørsteRender from '../../../hooks/useFørsteRender';
 import { ISøknadSpørsmål } from '../../../typer/søknad';
 import SpråkTekst from '../../Felleskomponenter/SpråkTekst/SpråkTekst';
 
 const useInputFelt = (
     søknadsfelt: ISøknadSpørsmål<string>,
     feilmeldingSpråkId: string,
-    skalVises: boolean
-) => {
-    const input = useFelt<string>({
+    skalVises = true
+) =>
+    useFelt<string>({
         feltId: søknadsfelt.id,
         verdi: søknadsfelt.svar,
         valideringsfunksjon: (felt: FeltState<string>) => {
@@ -22,12 +21,5 @@ const useInputFelt = (
         avhengigheter: { skalVises },
         skalFeltetVises: avhengigheter => avhengigheter.skalVises,
     });
-
-    useFørsteRender(() => {
-        input.verdi !== '' && input.validerOgSettFelt(input.verdi);
-    });
-
-    return input;
-};
 
 export default useInputFelt;
