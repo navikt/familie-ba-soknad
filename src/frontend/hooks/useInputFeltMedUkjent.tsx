a26a059feae12f853374f8ac5fcbb2034a9d60cf
Daphne Leebeek <daphne.leebeek@nav.no>
2021-09-09 12:14:54 +0200

    Utvid useInputFeltMedUkjent til å ta imot insendt valideringsfunksjon i tillegg til defaulten

--- src/frontend/hooks/useInputFeltMedUkjent.tsx
+++ src/frontend/hooks/useInputFeltMedUkjent.tsx
@@ -17,14 +17,15 @@ const useInputFeltMedUkjent = (
     avhengighet: Felt<ESvar>,
     feilmeldingSpråkId: string,
     erFnrInput = false,
-    skalVises = true
+    skalVises = true,
+    customValidering: ((felt: FeltState<string>) => FeltState<string>) | undefined = undefined
 ) => {
     const inputFelt = useFelt<ISODateString>({
         feltId: søknadsfelt ? søknadsfelt.id : guid(),
         verdi: søknadsfelt
             ? trimWhiteSpace(formaterInitVerdiForInputMedUkjent(søknadsfelt.svar))
             : '',
-        valideringsfunksjon: (felt: FeltState<string>, avhengigheter) => {
+        valideringsfunksjon: (felt: FeltState<string>, avhengigheter): FeltState<string> => {
             const feltVerdi = trimWhiteSpace(felt.verdi);
 
             if (
@@ -36,12 +37,18 @@ const useInputFeltMedUkjent = (
             }
 
             if (erFnrInput) {
-                return feltVerdi === '' || idnr(feltVerdi).status !== 'valid'
-                    ? feil(felt, <SpråkTekst id={feilmeldingSpråkId} />)
-                    : ok(felt);
+                if (feltVerdi === '') {
+                    return feil(felt, <SpråkTekst id={feilmeldingSpråkId} />);
+                } else if (idnr(feltVerdi).status !== 'valid') {
+                    return feil(felt, <SpråkTekst id={'felles.fnr.feil-format.feilmelding'} />);
+                } else {
+                    return customValidering ? customValidering(felt) : ok(felt);
+                }
             } else {
                 return feltVerdi !== ''
-                    ? ok(felt)
+                    ? customValidering
+                        ? customValidering(felt)
+                        : ok(felt)
                     : feil(felt, <SpråkTekst id={feilmeldingSpråkId} />);
             }
         },
