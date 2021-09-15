6cda28f9d798202cc32eee83b8dc5ef75a22761d
Daphne Leebeek <daphne.leebeek@nav.no>
2021-09-15 17:45:18 +0200

    Fjern midlertidig knapp

--- src/frontend/components/SøknadsSteg/Forside/Forside.tsx
+++ src/frontend/components/SøknadsSteg/Forside/Forside.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { useIntl } from 'react-intl';
 import styled from 'styled-components/macro';
 
-import { Knapp } from 'nav-frontend-knapper';
 import { Sidetittel } from 'nav-frontend-typografi';
 
 import { LocaleType, Sprakvelger } from '@navikt/familie-sprakvelger';
@@ -85,14 +84,6 @@ const Forside: React.FC = () => {
                     target="_blank"
                 />
             </Informasjonsbolk>
-            <Knapp
-                onClick={() => {
-                    throw Error();
-                }}
-                htmlType={'button'}
-            >
-                jeg feiler
-            </Knapp>
         </InnholdContainer>
     );
 };
