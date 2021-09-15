beba18cab23710a224ba2c27631fec91b9cb042b
norafutsaeter <nora.futseter@nav.no>
2021-09-15 12:54:33 +0200

    Frsøk på test

--- src/frontend/components/SøknadsSteg/OmBarnaDine/OmBarnaDine.test.tsx
+++ src/frontend/components/SøknadsSteg/OmBarnaDine/OmBarnaDine.test.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
 import { mockDeep } from 'jest-mock-extended';
+
+import { ESvar } from '@navikt/familie-form-elements';
 
 import { ISøknad } from '../../../typer/søknad';
 import {
     silenceConsoleErrors,
     spyOnUseApp,
+    TestProvidere,
     TestProvidereMedEkteTekster,
 } from '../../../utils/testing';
 import OmBarnaDine from './OmBarnaDine';
+import { OmBarnaDineSpørsmålId } from './spørsmål';
 
 jest.mock('react-router-dom', () => ({
     ...(jest.requireActual('react-router-dom') as object),
@@ -26,8 +30,13 @@ const søknad = mockDeep<ISøknad>({
     barnInkludertISøknaden: [
         {
             ident: '1234',
+            navn: 'Jens',
         },
     ],
+    erNoenAvBarnaFosterbarn: {
+        id: OmBarnaDineSpørsmålId.erNoenAvBarnaFosterbarn,
+        svar: ESvar.JA,
+    },
 });
 
 describe('OmBarnaDine', () => {
@@ -40,5 +49,22 @@ describe('OmBarnaDine', () => {
             </TestProvidereMedEkteTekster>
         );
         expect(console.error).toHaveBeenCalledTimes(0);
+    });
+
+    test('Kan huke av for barn i liste med checkbox', () => {
+        spyOnUseApp(søknad);
+
+        const { getByLabelText } = render(
+            <TestProvidere>
+                <OmBarnaDine />
+            </TestProvidere>
+        );
+        const checkbox: HTMLInputElement = getByLabelText(
+            /ombarna.fosterbarn.spm/
+        ) as HTMLInputElement;
+        expect(checkbox.checked).toBe(false);
+        act(() => checkbox.click());
+
+        expect(checkbox.checked).toBe(true);
     });
 });
