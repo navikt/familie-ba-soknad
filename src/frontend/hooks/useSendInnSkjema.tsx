20c13d599c60f00ec35afe9b65da3abfe6ea80e7
norafutsaeter <nora.futseter@nav.no>
2021-10-01 11:43:15 +0200

    oppdatere tekster i pdf

--- src/frontend/hooks/useSendInnSkjema.tsx
+++ src/frontend/hooks/useSendInnSkjema.tsx
@@ -190,18 +190,11 @@ export const useSendInnSkjema = (): {
             spørsmål: {
                 ...spørmålISøknadsFormat(typetBarnSpørsmål, { navn }),
                 ...spørmålISøknadsFormat(utvidet, { navn }),
-                [barnDataKeySpørsmål.søkerForTidsromStartdato]: søknadsfelt(
-                    språktekstIdFraSpørsmålId(OmBarnetSpørsmålsId.søkerForTidsromStartdato),
-                    sammeVerdiAlleSpråkEllerUkjentSpråktekst(
-                        søkerForTidsromStartdato.svar,
-                        'pdf.barn.ikke-oppgitt'
-                    )
-                ),
                 [barnDataKeySpørsmål.søkerForTidsromSluttdato]: søknadsfelt(
                     språktekstIdFraSpørsmålId(OmBarnetSpørsmålsId.søkerForTidsromSluttdato),
                     sammeVerdiAlleSpråkEllerUkjentSpråktekst(
                         søkerForTidsromSluttdato.svar,
-                        'pdf.barn.ikke-oppgitt'
+                        omBarnetSpørsmålSpråkId[OmBarnetSpørsmålsId.søkerForTidsromSluttdatoVetIkke]
                     )
                 ),
                 [barnDataKeySpørsmålUtvidet.søkerFlyttetFraAndreForelderDato]: søknadsfelt(
