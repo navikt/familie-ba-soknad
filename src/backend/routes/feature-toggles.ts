a69beddf5208f6872bd5d0ebbc354ff8ac4a1492
Bjørn Snoen <77009351+bjornsnoen-nav@users.noreply.github.com>
2021-10-04 14:12:51 +0200

    Feat/unleash proxy (#392)
    * (feat) Eksponere soknad-toggles for frontend
    
    * (fix) Bytt lokal api-port
    
    * (chore) Formell enum for kjente feature-toggles, fjern log

---
+++ src/backend/routes/feature-toggles.ts
@@ -0,0 +1,24 @@
+import { Express, RequestHandler } from 'express';
+
+import { byggFeiletRessurs, byggSuksessRessurs } from '@navikt/familie-typer';
+
+import { EFeatureToggle } from '../../frontend/typer/feature-toggles';
+import { basePath } from '../environment';
+import { isEnabled } from '../utils/unleash';
+
+const toggleFetchHandler: RequestHandler = (req, res) => {
+    const toggleId = req.params.id;
+    if (!toggleId) {
+        res.status(404).send(byggFeiletRessurs('Mangler toggle-id'));
+        return;
+    }
+
+    res.send(byggSuksessRessurs(isEnabled(toggleId as EFeatureToggle)));
+};
+
+export const konfigurerFeatureTogglesEndpoint = (app: Express): Express => {
+    // Matcher bare toggles som tilhører oss, bruker {0,} pga en express-quirk
+    // ref http://expressjs.com/en/guide/routing.html#route-parameters
+    app.get(`${basePath}toggles/:id(familie-ba-soknad.[a-zA-Z-]{0,})`, toggleFetchHandler);
+    return app;
+};
