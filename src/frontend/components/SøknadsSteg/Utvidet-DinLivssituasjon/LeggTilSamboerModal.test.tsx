bbfe39be7eaf4759a1aae90b8a5da332ed6cd5d7
norafutsaeter <nora.futseter@nav.no>
2021-10-13 11:13:15 +0200

    Teste feiloppsummeringskomponent

---
+++ src/frontend/components/SøknadsSteg/Utvidet-DinLivssituasjon/LeggTilSamboerModal.test.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import { render, within } from '@testing-library/react';
+import { mockDeep } from 'jest-mock-extended';
+import { act } from 'react-dom/test-utils';
+
+import { ESøknadstype, ISøknad } from '../../../typer/søknad';
+import {
+    mockHistory,
+    silenceConsoleErrors,
+    spyOnUseApp,
+    TestProvidereMedEkteTekster,
+} from '../../../utils/testing';
+import DinLivssituasjon from './DinLivssituasjon';
+
+const søknad = mockDeep<ISøknad>({
+    søknadstype: ESøknadstype.UTVIDET,
+    barnInkludertISøknaden: [
+        {
+            ident: '1234',
+        },
+    ],
+    søker: {
+        utvidet: {
+            nåværendeSamboer: null,
+        },
+    },
+});
+
+describe('LeggTilSamboerModal', () => {
+    mockHistory(['/din-livssituasjon']);
+
+    beforeEach(() => {
+        silenceConsoleErrors();
+    });
+    it('Viser riktige feilmeldinger ved ingen utfylte felt av tidligere samboer', () => {
+        spyOnUseApp(søknad);
+
+        const { getByText, getByRole, getAllByText } = render(
+            <TestProvidereMedEkteTekster>
+                <DinLivssituasjon />
+            </TestProvidereMedEkteTekster>
+        );
+
+        const leggTilSamboerKnapp: HTMLElement = getByText('Legg til samboer');
+        act(() => leggTilSamboerKnapp.click());
+
+        const gåVidereKnapp = getAllByText('Legg til samboer');
+        act(() => gåVidereKnapp[2].click());
+
+        const feiloppsummering = getByRole('alert');
+
+        const navnFeilmelding = within(feiloppsummering).getByText(
+            'Du må oppgi samboerens navn for å gå videre'
+        );
+        expect(navnFeilmelding).toBeInTheDocument();
+        const fødselsnummerFeilmelding = within(feiloppsummering).getByText(
+            'Du må oppgi samboerens fødselsnummer for å gå videre'
+        );
+        expect(fødselsnummerFeilmelding).toBeInTheDocument();
+        const forholdStartFeilmelding = within(feiloppsummering).getByText(
+            'Du må oppgi når samboerforholdet startet for å gå videre'
+        );
+        expect(forholdStartFeilmelding).toBeInTheDocument();
+        const forholdSluttFeilmelding = within(feiloppsummering).getByText(
+            'Du må oppgi når samboerforholdet ble avsluttet for å gå videre'
+        );
+        expect(forholdSluttFeilmelding).toBeInTheDocument();
+    });
+});
