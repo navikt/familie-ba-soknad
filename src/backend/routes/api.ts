81b9ffc432d55a75365d128d90923d12c06c7275
Bjørn Snoen <77009351+bjornsnoen-nav@users.noreply.github.com>
2021-10-18 11:03:44 +0200

    Deps/typescript (#432)
    * Bump ts-essentials from 7.0.3 to 9.0.0
    
    Bumps [ts-essentials](https://github.com/krzkaczor/ts-essentials) from 7.0.3 to 9.0.0.
    - [Release notes](https://github.com/krzkaczor/ts-essentials/releases)
    - [Changelog](https://github.com/krzkaczor/ts-essentials/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/krzkaczor/ts-essentials/compare/v7.0.3...v9.0.0)
    
    ---
    updated-dependencies:
    - dependency-name: ts-essentials
      dependency-type: direct:development
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump @types/node from 14.14.22 to 16.11.0
    
    Bumps [@types/node](https://github.com/DefinitelyTyped/DefinitelyTyped/tree/HEAD/types/node) from 14.14.22 to 16.11.0.
    - [Release notes](https://github.com/DefinitelyTyped/DefinitelyTyped/releases)
    - [Commits](https://github.com/DefinitelyTyped/DefinitelyTyped/commits/HEAD/types/node)
    
    ---
    updated-dependencies:
    - dependency-name: "@types/node"
      dependency-type: direct:development
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Fiks en type
    
    * (fix) En bunch type errors i backend
    
    * (fix) bodyparser-import
    
    * (chore) Bruk bodyparser bundlet med express
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Daphne <daphne.leebeek@nav.no>

--- src/backend/routes/api.ts
+++ src/backend/routes/api.ts
@@ -1,4 +1,5 @@
 import express, { Express } from 'express';
+import { RequestHandler } from 'http-proxy-middleware';
 
 import { basePath } from '../environment';
 import { erklaeringInterceptor } from '../middlewares/erklaering-interceptor';
@@ -7,7 +8,7 @@ import { createApiForwardingFunction } from '../middlewares/proxy';
 
 export const konfigurerApi = (app: Express): Express => {
     // Sett opp middleware for input-sanitering
-    app.use(`${basePath}api/soknad`, express.json());
+    app.use(`${basePath}api/soknad`, express.json() as RequestHandler);
     app.use(`${basePath}api/soknad`, erklaeringInterceptor);
     app.use(`${basePath}api/soknad`, escapeBody);
     app.use(`${basePath}api`, createApiForwardingFunction());
