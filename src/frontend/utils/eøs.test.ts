e3d1aa5c4d78bcde3df5084ce2f6238ebd7e0930
Bjørn Snoen <bjorn.magne.snoen@nav.no>
2021-11-16 10:31:07 +0100

    (feat) Vis eøs-notis på første uperiode som er i eøs-land

--- src/frontend/utils/eøs.test.ts
+++ src/frontend/utils/eøs.test.ts
@@ -1,6 +1,8 @@
 import { Alpha3Code } from 'i18n-iso-countries';
+import { mockDeep } from 'jest-mock-extended';
 
 import { OmBarnetSpørsmålsId } from '../components/SøknadsSteg/OmBarnet/spørsmål';
+import { IUtenlandsperiode } from '../typer/person';
 import { ISøknad } from '../typer/søknad';
 import { landSvarSomKanTriggeEøs } from './eøs';
 import { mekkGyldigSøknad } from './testing';
@@ -15,6 +17,9 @@ describe('eøs', () => {
                     ...søknad.søker,
                     arbeidsland: { ...søknad.søker.arbeidsland, svar: 'ALA' },
                     pensjonsland: { ...søknad.søker.pensjonsland, svar: 'ALA' },
+                    utenlandsperioder: [
+                        mockDeep<IUtenlandsperiode>({ oppholdsland: { svar: 'FIN' } }),
+                    ],
                 },
                 barnInkludertISøknaden: [
                     {
@@ -38,6 +43,7 @@ describe('eøs', () => {
             expect(landSvarSomKanTriggeEøs(mockSøknad)).toEqual([
                 'ALA',
                 'ALA',
+                'FIN',
                 'BEL',
                 'DEU',
                 'DEU',
