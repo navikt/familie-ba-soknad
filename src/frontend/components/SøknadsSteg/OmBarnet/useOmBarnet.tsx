82c44943cfbb6c316036b7f09b3d82155269d112
Daphne Leebeek <daphne.leebeek@nav.no>
2021-11-22 15:36:31 +0100

    Egen feilmelding for tomt postnummer

--- src/frontend/components/SøknadsSteg/OmBarnet/useOmBarnet.tsx
+++ src/frontend/components/SøknadsSteg/OmBarnet/useOmBarnet.tsx
@@ -102,14 +102,14 @@ export const useOmBarnet = (
         verdi: barn[barnDataKeySpørsmål.institusjonspostnummer].svar,
         feltId: barn[barnDataKeySpørsmål.institusjonspostnummer].id,
         valideringsfunksjon: felt =>
-            regexNorskEllerUtenlandskPostnummer(felt.verdi)
+            regexNorskEllerUtenlandskPostnummer(trimWhiteSpace(felt.verdi))
                 ? ok(felt)
                 : feil(
                       felt,
                       <SpråkTekst
                           id={
-                              felt.verdi.length > 10
-                                  ? 'ombarnet.institusjon.postnummer.over-ti-tegn.feilmelding'
+                              trimWhiteSpace(felt.verdi) === ''
+                                  ? 'ombarnet.institusjon.postnummer.feilmelding'
                                   : 'ombarnet.institusjon.postnummer.under-tre-tegn.feilmelding'
                           }
                       />
