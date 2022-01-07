5f3a396a84c4f97d0a66e7ccb16d8bddf45715a1
André Wolden <wolden.andre@gmail.com>
2022-01-07 15:25:59 +0100

    retter BlokkerTilbakeKnappModal.test

--- src/frontend/components/Felleskomponenter/BlokkerTilbakeKnappModal/BlokkerTilbakeKnappModal.test.tsx
+++ src/frontend/components/Felleskomponenter/BlokkerTilbakeKnappModal/BlokkerTilbakeKnappModal.test.tsx
@@ -3,8 +3,10 @@ import React from 'react';
 import { act, render, waitFor } from '@testing-library/react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import { IBarn, IBarnRespons } from '../../../typer/person';
-import { ISøknad } from '../../../typer/søknad';
+import { ESvar } from '@navikt/familie-form-elements';
+
+import { barnDataKeySpørsmål, IBarn, IBarnRespons } from '../../../typer/person';
+import { IBarnMedISøknad } from '../../../typer/søknad';
 import * as eøsUtils from '../../../utils/eøs';
 import {
     mockEøs,
@@ -13,6 +15,8 @@ import {
     spyOnUseApp,
     TestProvidere,
 } from '../../../utils/testing';
+import { OmBarnaDineSpørsmålId } from '../../SøknadsSteg/OmBarnaDine/spørsmål';
+import { OmBarnetSpørsmålsId } from '../../SøknadsSteg/OmBarnet/spørsmål';
 import BlokkerTilbakeKnappModal from './BlokkerTilbakeKnappModal';
 
 const manueltRegistrert: Partial<IBarn> = {
@@ -22,6 +26,34 @@ const manueltRegistrert: Partial<IBarn> = {
 const fraPdl: Partial<IBarnRespons> = {
     ident: '54321',
     navn: 'B',
+};
+
+const manueltRegistrertSomIBarnMedISøknad: Partial<IBarnMedISøknad> = {
+    ...manueltRegistrert,
+    id: 'random-id-1',
+    barnetrygdFraEøslandHvilketLand: {
+        id: OmBarnetSpørsmålsId.barnetrygdFraEøslandHvilketLand,
+        svar: 'NOR',
+    },
+    utenlandsperioder: [],
+    [barnDataKeySpørsmål.barnetrygdFraAnnetEøsland]: {
+        id: OmBarnaDineSpørsmålId.mottarBarnetrygdForBarnFraAnnetEøsland,
+        svar: ESvar.NEI,
+    },
+};
+
+const fraPdlSomIBarnMedISøknad: Partial<IBarnMedISøknad> = {
+    ...fraPdl,
+    id: 'random-id-2',
+    barnetrygdFraEøslandHvilketLand: {
+        id: OmBarnetSpørsmålsId.barnetrygdFraEøslandHvilketLand,
+        svar: 'NOR',
+    },
+    utenlandsperioder: [],
+    [barnDataKeySpørsmål.barnetrygdFraAnnetEøsland]: {
+        id: OmBarnaDineSpørsmålId.mottarBarnetrygdForBarnFraAnnetEøsland,
+        svar: ESvar.NEI,
+    },
 };
 
 describe('Ingen navigering tilbake til søknad fra kvitteringssiden', () => {
@@ -34,13 +66,13 @@ describe('Ingen navigering tilbake til søknad fra kvitteringssiden', () => {
         silenceConsoleErrors();
         const søknad = {
             barnRegistrertManuelt: [manueltRegistrert],
-            barnInkludertISøknaden: [manueltRegistrert, fraPdl],
+            barnInkludertISøknaden: [manueltRegistrertSomIBarnMedISøknad, fraPdlSomIBarnMedISøknad],
             søker: { barn: [fraPdl] },
             dokumentasjon: [],
         };
         const { settSøknad } = spyOnUseApp(søknad);
 
-        settSøknad.mockImplementation((nySøknad: ISøknad) => {
+        settSøknad.mockImplementation(nySøknad => {
             søknad.barnRegistrertManuelt = nySøknad.barnRegistrertManuelt;
             søknad.barnInkludertISøknaden = nySøknad.barnInkludertISøknaden;
         });
