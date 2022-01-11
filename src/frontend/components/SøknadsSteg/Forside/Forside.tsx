797ed2b19972a5e1f620b49efe2cbe93d0826cc3
Daphne Leebeek <daphne.leebeek@nav.no>
2022-01-11 17:31:58 +0100

    Oppdater nb-fil

--- src/frontend/components/SøknadsSteg/Forside/Forside.tsx
+++ src/frontend/components/SøknadsSteg/Forside/Forside.tsx
@@ -67,14 +67,7 @@ const Forside: React.FC = () => {
 
             <StyledSpråkvelger støttedeSprak={[LocaleType.nn, LocaleType.nb, LocaleType.en]} />
             <Informasjonsbolk>
-                <SpråkTekst
-                    id={
-                        eøsSkruddAv
-                            ? 'forside.info.punktliste.med-eøs-info'
-                            : 'forside.info.punktliste'
-                    }
-                    values={{ b: msg => <b>{msg}</b> }}
-                />
+                <SpråkTekst id={'forside.info.punktliste'} values={{ b: msg => <b>{msg}</b> }} />
                 <EksternLenke
                     lenkeSpråkId={'forside.plikter.lenke'}
                     lenkeTekstSpråkId={'forside.plikter.lenketekst'}
