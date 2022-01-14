e8622079f4576a3886bb54a11247fb7f112ab993
André Wolden <wolden.andre@gmail.com>
2022-01-14 10:49:27 +0100

    retter

--- src/scripts/find-missing-text-ids.js
+++ src/scripts/find-missing-text-ids.js
@@ -8,6 +8,7 @@ const nb = require('../frontend/assets/lang/nb.json');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const nn = require('../frontend/assets/lang/nn.json');
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const finnMuligensUbrukteIder = (alleBenyttedeSpråkIder, språkfil, språkfilNavn) => {
     console.info('------------------------------------------------------------');
     console.info(`ubrukte språkId'er i ${språkfilNavn}.json: 
@@ -33,9 +34,9 @@ const finnMuligensUbrukteIder = (alleBenyttedeSpråkIder, språkfil, språkfilNa
 exec(
     `grep -r -oh -I --exclude '.json' --exclude '.test.ts' -E "([\\'|\\"]{1,1}([a-zA-ZæøåÆØÅ]+\\.{1,1})(([a-zA-ZæøåÆØÅ]+\\.{1,1})|([a-zA-ZæøåÆØÅ]+\\_{1,1})|([a-zA-ZæøåÆØÅ]+\\-{1,1}))*[a-zA-ZæøåÆØÅ]+[\\'|\\"]{1,1})" ./src/frontend`,
     (_, stdout) => {
-        let ids = stdout.split('\n');
+        let alleMatchendeIder = stdout.split('\n');
 
-        let alleBenyttedeSpråkIder = [...new Set(ids)]
+        let alleBenyttedeSpråkIder = [...new Set(alleMatchendeIder)]
             .map(elem => {
                 return elem.slice(1, elem.length - 1);
             })
@@ -53,15 +54,39 @@ exec(
                 );
             });
 
-        const iderSomIkkeErIBrukNB = finnMuligensUbrukteIder(alleBenyttedeSpråkIder, nb, 'NB');
-        const iderSomIkkeErIBrukNN = finnMuligensUbrukteIder(alleBenyttedeSpråkIder, nn, 'NN');
-        const iderSomIkkeErIBrukEN = finnMuligensUbrukteIder(alleBenyttedeSpråkIder, en, 'EN');
+        console.info('------------------------------------------------------------');
+        console.info('------------------------------------------------------------');
+        console.info("språkid'er som mangler i nb.json: \n");
+        alleBenyttedeSpråkIder.forEach(elem => {
+            if (!(elem in nb)) {
+                console.info(`ERROR -> ${elem}`);
+            }
+        });
 
         console.info('------------------------------------------------------------');
-        console.info(`nb length: ${Object.keys(nb).length}`);
-        console.info(`antall benyttede språknøkkler: ${alleBenyttedeSpråkIder.length}`);
-        console.info(`Ider som ikke er i bruk fra nb.json: ${iderSomIkkeErIBrukNB.length}`);
-        console.info(`Ider som ikke er i bruk fra nn.json: ${iderSomIkkeErIBrukNN.length}`);
-        console.info(`Ider som ikke er i bruk fra en.json: ${iderSomIkkeErIBrukEN.length}`);
+        console.info("språkid'er som mangler i nn.json: \n");
+        alleBenyttedeSpråkIder.forEach(elem => {
+            if (!(elem in nn)) {
+                console.info(`ERROR -> ${elem}`);
+            }
+        });
+        console.info('------------------------------------------------------------');
+        console.info("språkid'er som mangler i en.json: \n");
+        alleBenyttedeSpråkIder.forEach(elem => {
+            if (!(elem in en)) {
+                console.info(`ERROR -> ${elem}`);
+            }
+        });
+
+        // console.info('------------------------------------------------------------');
+        // const iderSomIkkeErIBrukNB = finnMuligensUbrukteIder(alleBenyttedeSpråkIder, nb, 'NB');
+        // const iderSomIkkeErIBrukNN = finnMuligensUbrukteIder(alleBenyttedeSpråkIder, nn, 'NN');
+        // const iderSomIkkeErIBrukEN = finnMuligensUbrukteIder(alleBenyttedeSpråkIder, en, 'EN');
+
+        // console.info(`nb length: ${Object.keys(nb).length}`);
+        // console.info(`antall benyttede språknøkkler: ${alleBenyttedeSpråkIder.length}`);
+        // console.info(`Ider som ikke er i bruk fra nb.json: ${iderSomIkkeErIBrukNB.length}`);
+        // console.info(`Ider som ikke er i bruk fra nn.json: ${iderSomIkkeErIBrukNN.length}`);
+        // console.info(`Ider som ikke er i bruk fra en.json: ${iderSomIkkeErIBrukEN.length}`);
     }
 );
