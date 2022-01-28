c2348a7e86475dfd83c6e6f259b84c952e9adb3d
Daphne <daphne.leebeek@nav.no>
2022-01-28 12:18:15 +0100

    Feature/oppsummering (#569)
    * Legg til eøs-steg i oppsummering
    
    * FIks liten UI-feil
    
    * Store bokstaver pirk
    
    * Oppsummeringsfelt som ligger på barn
    
    * Legg til arbeidsperioder for søker på oppsummering
    
    * Fiks enketekst feil på arbeidsperiode

--- src/frontend/components/SøknadsSteg/Oppsummering/OppsummeringSteg/EøsSøkerOppsummering.tsx
+++ src/frontend/components/SøknadsSteg/Oppsummering/OppsummeringSteg/EøsSøkerOppsummering.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import { ESvar } from '@navikt/familie-form-elements';
+
+import { useApp } from '../../../../context/AppContext';
 import { useRoutes } from '../../../../context/RoutesContext';
 import { RouteEnum } from '../../../../typer/routes';
+import { ISøknadSpørsmål } from '../../../../typer/spørsmål';
+import { ArbeidsperiodeOppsummering } from '../../../Felleskomponenter/Arbeidsperiode/ArbeidsperiodeOppsummering';
+import SpråkTekst from '../../../Felleskomponenter/SpråkTekst/SpråkTekst';
+import { eøsSøkerSpørsmålSpråkId } from '../../EøsSteg/Søker/spørsmål';
 import { useEøsForSøker } from '../../EøsSteg/Søker/useEøsForSøker';
+import { OppsummeringFelt } from '../OppsummeringFelt';
 import Oppsummeringsbolk from '../Oppsummeringsbolk';
+import { StyledOppsummeringsFeltGruppe } from '../OppsummeringsFeltGruppe';
 
 interface Props {
     settFeilAnchors: React.Dispatch<React.SetStateAction<string[]>>;
@@ -11,6 +20,15 @@ interface Props {
 
 const EøsSøkerOppsummering: React.FC<Props> = ({ settFeilAnchors }) => {
     const { hentRouteObjektForRouteEnum } = useRoutes();
+    const { søknad } = useApp();
+    const søker = søknad.søker;
+
+    const jaNeiSpmOppsummering = (søknadSpørsmål: ISøknadSpørsmål<ESvar | null>) => (
+        <OppsummeringFelt
+            tittel={<SpråkTekst id={eøsSøkerSpørsmålSpråkId[søknadSpørsmål.id]} />}
+            søknadsvar={søknadSpørsmål.svar}
+        />
+    );
 
     return (
         <Oppsummeringsbolk
@@ -18,7 +36,18 @@ const EøsSøkerOppsummering: React.FC<Props> = ({ settFeilAnchors }) => {
             tittel={'eøs-om-deg.sidetittel'}
             skjemaHook={useEøsForSøker}
             settFeilAnchors={settFeilAnchors}
-        />
+        >
+            <StyledOppsummeringsFeltGruppe>
+                {jaNeiSpmOppsummering(søker.arbeidINorge)}
+                {søker.arbeidsperioderNorge.map((arbeidsperiode, index) => (
+                    <ArbeidsperiodeOppsummering
+                        key={`arbeidsperiode-søker-norge-${index}`}
+                        arbeidsperiode={arbeidsperiode}
+                        nummer={index + 1}
+                    />
+                ))}
+            </StyledOppsummeringsFeltGruppe>
+        </Oppsummeringsbolk>
     );
 };
 
