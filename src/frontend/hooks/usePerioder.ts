d16d3bbffc9b936e8214bc4177417baf3f765876
norafutsaeter <nora.futseter@nav.no>
2022-01-31 12:51:36 +0100

    felt blir validert på nytt om avhengig felt endret

--- src/frontend/hooks/usePerioder.ts
+++ src/frontend/hooks/usePerioder.ts
@@ -27,7 +27,7 @@ export const usePerioder = <T>(
 
     useEffect(() => {
         registrertePerioder.validerOgSettFelt(perioder);
-    }, [perioder]);
+    }, [perioder, avhengigheter]);
 
     return {
         leggTilPeriode,
