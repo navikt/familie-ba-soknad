4fea2c554b233d9b2944704c8df8e66d2f48fff3
norafutsaeter <nora.futseter@nav.no>
2022-03-23 13:46:25 +0100

    refactor dropdown

--- src/frontend/components/Felleskomponenter/Dropdowns/SlektsforholdDropdown.tsx
+++ src/frontend/components/Felleskomponenter/Dropdowns/SlektsforholdDropdown.tsx
@@ -1,78 +1,35 @@
-import React, { PropsWithChildren, ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 
 import { useIntl } from 'react-intl';
-import styled from 'styled-components';
-
-import { Select } from 'nav-frontend-skjema';
-
-import { Felt, ISkjema } from '@navikt/familie-skjema';
 
 import { muligeSlektsforhold } from '../../../typer/barn';
 import { Slektsforhold } from '../../../typer/kontrakt/barn';
-import { SkjemaFeltTyper } from '../../../typer/skjema';
 import { toSlektsforholdSpråkId } from '../../../utils/språk';
+import StyledDropdown, { StyledDropdownProps } from './StyledDropdown';
 
-interface StyledSlektsforholdDropdownProps<ConstrainedString extends string> {
-    felt: Felt<ConstrainedString>;
-    skjema: ISkjema<SkjemaFeltTyper, string>;
-    placeholder: string;
-    label?: ReactNode;
-    dynamisk?: boolean;
-    bredde?: 'fullbredde' | 'xxl' | 'xl' | 'l' | 'm' | 's' | 'xs';
+export interface SlektsforholdDropdownProps extends StyledDropdownProps<Slektsforhold | ''> {
     gjelderSøker?: boolean;
 }
 
-const StyledSelect = styled(Select)`
-    label {
-        font-size: 1.125rem;
-    }
-`;
-
-const StyledSlektsforholdDropdown = <ConstrainedString extends string>({
-    children,
-    felt,
-    skjema,
-    placeholder,
-    label,
-    dynamisk = false,
-    bredde,
-}: PropsWithChildren<StyledSlektsforholdDropdownProps<ConstrainedString>>) =>
-    felt.erSynlig ? (
-        <div id={felt.id} aria-live={dynamisk ? 'polite' : 'off'}>
-            <StyledSelect
-                label={label}
-                {...felt.hentNavInputProps(skjema.visFeilmeldinger)}
-                id={undefined}
-                bredde={bredde || 'l'}
-            >
-                <option disabled={true} value={''}>
-                    {placeholder}
-                </option>
-                {children}
-                <optgroup
-                    label="" /* En tom optgroup hindrer teksten i dropdown på ios å bli truncated */
-                />
-            </StyledSelect>
-        </div>
-    ) : null;
-
-const SlektsforholdDropdown: React.FC<StyledSlektsforholdDropdownProps<Slektsforhold | ''>> =
-    props => {
-        const intl = useIntl();
-        const aktuelleSlektsforhold = props.gjelderSøker
-            ? muligeSlektsforhold
-            : muligeSlektsforhold.filter(slektsforhold => slektsforhold !== Slektsforhold.FORELDER);
-        return (
-            <StyledSlektsforholdDropdown<Slektsforhold | ''> {...props} bredde={'fullbredde'}>
-                {aktuelleSlektsforhold.map(
-                    (slektsforhold): ReactNode => (
-                        <option value={slektsforhold} key={slektsforhold}>
-                            {intl.formatMessage({ id: toSlektsforholdSpråkId(slektsforhold) })}
-                        </option>
-                    )
-                )}
-            </StyledSlektsforholdDropdown>
-        );
-    };
+const SlektsforholdDropdown: React.FC<SlektsforholdDropdownProps> = ({
+    gjelderSøker = false,
+    ...props
+}) => {
+    const intl = useIntl();
+    const aktuelleSlektsforhold = gjelderSøker
+        ? muligeSlektsforhold
+        : muligeSlektsforhold.filter(slektsforhold => slektsforhold !== Slektsforhold.FORELDER);
+    return (
+        <StyledDropdown<Slektsforhold | ''> {...props} bredde={'fullbredde'}>
+            {aktuelleSlektsforhold.map(
+                (slektsforhold): ReactNode => (
+                    <option value={slektsforhold} key={slektsforhold}>
+                        {intl.formatMessage({ id: toSlektsforholdSpråkId(slektsforhold) })}
+                    </option>
+                )
+            )}
+        </StyledDropdown>
+    );
+};
 
 export default SlektsforholdDropdown;
