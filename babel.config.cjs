d181ef9b1dd013aa2a62a7b8f8d857e99ad0ca57
Meng Zhu <meng.zhu@nav.no>
2022-05-05 11:45:59 +0200

    try fix node error

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,9 +1,8 @@
 // Jest nekter å lese .babelrc-filer, så da får det bli babel.config.cjs
 module.exports = {
     presets: [
-        'react-app',
-        "@babel/preset-env",
-        "@babel/preset-react"],
+        'react-app'
+    ],
     plugins: [
         [
             'babel-plugin-styled-components',
