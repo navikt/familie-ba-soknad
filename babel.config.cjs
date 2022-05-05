f9e19e10e507120bc1158a79f752aa4106cc2939
Meng Zhu <meng.zhu@nav.no>
2022-05-05 11:57:54 +0200

    try fix node error

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,15 +1,21 @@
 // Jest nekter å lese .babelrc-filer, så da får det bli babel.config.cjs
 module.exports = {
-    presets: [
-        'react-app'
-    ],
+    presets: ['@babel/preset-env', '@babel/preset-react'],
     plugins: [
         [
             'babel-plugin-styled-components',
             {
                 displayName: process.env.NODE_ENV !== 'production',
             },
-            "@babel/plugin-syntax-import-assertions",
+            '@babel/plugin-syntax-import-assertions',
+            [
+                'transform-react-remove-prop-types',
+                {
+                    mode: 'remove',
+                    removeImport: 'true',
+                    ignoreFilenames: ['node_modules'],
+                },
+            ],
         ],
     ],
 };
