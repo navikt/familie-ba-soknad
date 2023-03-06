18a06e6a8e1ea873f02f84fa876604e575140a7d
Daphne Leebeek <daphne.leebeek@nav.no>
2023-03-06 11:28:20 +0100

    Fjern test som egentlig tester om det finnes console.error fremfor hva den faktisk sier.

--- src/frontend/components/SøknadsSteg/Forside/Forside.test.tsx
+++ src/frontend/components/SøknadsSteg/Forside/Forside.test.tsx
@@ -1,18 +1,7 @@
-import React from 'react';
-
-import { render } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
-
 import navFarger from 'nav-frontend-core';
 
-import {
-    mockEøs,
-    mockHistory,
-    spyOnUseApp,
-    TestProvidereMedEkteTekster,
-} from '../../../utils/testing';
+import { mockEøs, mockHistory, spyOnUseApp } from '../../../utils/testing';
 import { bekreftelseBoksBorderFarge } from './BekreftelseOgStartSoknad';
-import Forside from './Forside';
 import { BekreftelseStatus } from './useBekreftelseOgStartSoknad';
 
 describe('Forside', () => {
@@ -21,17 +10,6 @@ describe('Forside', () => {
         mockHistory(['/']);
         spyOnUseApp({});
         mockEøs();
-    });
-
-    test('Alle tekster finnes i språkfil', async () => {
-        await act(async () => {
-            render(
-                <TestProvidereMedEkteTekster>
-                    <Forside />
-                </TestProvidereMedEkteTekster>
-            );
-        });
-        expect(console.error).toHaveBeenCalledTimes(0);
     });
 
     test('Return riktig borderfarge basert på status', () => {
