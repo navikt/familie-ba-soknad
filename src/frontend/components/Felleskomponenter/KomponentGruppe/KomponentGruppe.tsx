dfb549aab6acaf023e50d1465a9a8ed6f4942626
Kristine Steine <kristine.steine@bekk.no>
2023-08-18 16:01:02 +0200

    Unngå at props til styled comp blir videreført til DOMen

--- src/frontend/components/Felleskomponenter/KomponentGruppe/KomponentGruppe.tsx
+++ src/frontend/components/Felleskomponenter/KomponentGruppe/KomponentGruppe.tsx
@@ -2,9 +2,9 @@ import React, { ReactNode } from 'react';
 
 import styled from 'styled-components';
 
-const Container = styled.div<{ inline: boolean }>`
+const Container = styled.div<{ $inline: boolean }>`
     && {
-        margin-bottom: ${props => (props.inline ? '2rem' : '4rem')};
+        margin-bottom: ${props => (props.$inline ? '2rem' : '4rem')};
     }
 
     > div :not(:last-child) {
@@ -19,7 +19,7 @@ const KomponentGruppe: React.FC<{
     children?: ReactNode;
 }> = ({ className, inline = false, dynamisk = false, children }) => {
     return (
-        <Container inline={inline} className={className} aria-live={dynamisk ? 'polite' : 'off'}>
+        <Container $inline={inline} className={className} aria-live={dynamisk ? 'polite' : 'off'}>
             {React.Children.map(children, child => {
                 return child && <div>{child}</div>;
             })}
