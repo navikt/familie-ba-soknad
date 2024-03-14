5d9b5d6b43dba96ad2b929521643fe0fcdc467c4
Kristine Steine <kristine.steine@nav.no>
2024-03-14 13:52:18 +0100

    Legg til kill switch for kombinert søknadsdialog (#1132)
    * Legg til kill switch for kombinert søknadsdialog
    
    * Rett opp skrivefeil i toggle-ID
    
    * Fjern gamle kill switches og fjern feature toggle foran den nye

--- src/backend/middlewares/feature-toggles.ts
+++ src/backend/middlewares/feature-toggles.ts
@@ -8,15 +8,12 @@ import { isEnabled } from '../utils/unleash';
  */
 export const expressToggleInterceptor: RequestHandler = (req, res, next) => {
     const språk: string | undefined = req.cookies['decorator-language'];
-    const erUtvidet = req.url.includes('utvidet');
 
     let skalRendreDisabledApp;
     if (process.env.FORCE_DISABLED) {
         skalRendreDisabledApp = true;
-    } else if (erUtvidet) {
-        skalRendreDisabledApp = isEnabled(EKillSwitchToggle.UTVIDET);
     } else {
-        skalRendreDisabledApp = isEnabled(EKillSwitchToggle.ORDINAER);
+        skalRendreDisabledApp = isEnabled(EKillSwitchToggle.SOKNAD);
     }
     skalRendreDisabledApp ? res.render('disabled.html', { LOCALE_CODE: språk ?? 'nb' }) : next();
 };
