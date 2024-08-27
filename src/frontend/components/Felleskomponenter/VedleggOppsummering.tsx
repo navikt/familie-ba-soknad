a4bae83c67c437045fc011b22a201c7c58295f08
ole-kristian-rudjord <olekristianrudjord@gmail.com>
2024-08-27 09:21:37 +0200

    Legger til 'lastOppSenereISoknad' tekst i VedleggOppsummering

--- src/frontend/components/Felleskomponenter/VedleggOppsummering.tsx
+++ src/frontend/components/Felleskomponenter/VedleggOppsummering.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 
 import { Alert, List } from '@navikt/ds-react';
 
+import { useApp } from '../../context/AppContext';
 import { dokumentasjonsbehovTilSpråkId } from '../../typer/dokumentasjon';
 import { Dokumentasjonsbehov } from '../../typer/kontrakt/dokumentasjon';
 
@@ -15,12 +16,17 @@ interface IVedleggOppsummeringProps {
 }
 
 export const VedleggOppsummering: FC<IVedleggOppsummeringProps> = ({ vedlegg }) => {
+    const { tekster, plainTekst } = useApp();
+
+    const dokumentasjonTekster = tekster().DOKUMENTASJON;
+
     const vedleggSomSkalVises = vedlegg.filter(vedlegg => vedlegg.skalVises);
 
     return (
         <>
             {vedleggSomSkalVises.length > 0 && (
                 <Alert variant="info">
+                    {plainTekst(dokumentasjonTekster.lastOppSenereISoknad)}
                     <List>
                         {vedleggSomSkalVises.map((vedlegg, index) => (
                             <List.Item key={index}>
