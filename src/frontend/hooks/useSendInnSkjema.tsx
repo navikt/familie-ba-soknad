db54e80036858b9d2dd959527675108f453e147b
Hanne Olsen <hanne.olsen@nav.no>
2024-08-29 15:27:35 +0200

    Legger til Sentry-varslinger hvis noe feiler på innsending av søknad, samme som for KS

--- src/frontend/hooks/useSendInnSkjema.tsx
+++ src/frontend/hooks/useSendInnSkjema.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/react';
 import { AxiosError } from 'axios';
 
 import { RessursStatus } from '@navikt/familie-typer';
@@ -17,25 +18,34 @@ export const useSendInnSkjema = (): {
     const { soknadApiProxyUrl } = Miljø();
     const { valgtLocale } = useSpråk();
     const sendInnSkjemaV8 = async (): Promise<[boolean, ISøknadKontraktV8]> => {
-        settInnsendingStatus({ status: RessursStatus.HENTER });
+        try {
+            settInnsendingStatus({ status: RessursStatus.HENTER });
 
-        const formatert: ISøknadKontraktV8 = dataISøknadKontraktFormatV8(valgtLocale, søknad);
+            const formatert: ISøknadKontraktV8 = dataISøknadKontraktFormatV8(valgtLocale, søknad);
 
-        const res = await sendInn<ISøknadKontraktV8>(
-            formatert,
-            axiosRequest,
-            `${soknadApiProxyUrl}/soknad/v8`,
-            (res: AxiosError) => {
-                const responseData = res.response?.data;
-                if (responseData && erModellMismatchResponsRessurs(responseData)) {
-                    settSisteModellVersjon(responseData.data.modellVersjon);
+            const res = await sendInn<ISøknadKontraktV8>(
+                formatert,
+                axiosRequest,
+                `${soknadApiProxyUrl}/soknad/v8`,
+                (res: AxiosError) => {
+                    const responseData = res.response?.data;
+                    if (responseData && erModellMismatchResponsRessurs(responseData)) {
+                        settSisteModellVersjon(responseData.data.modellVersjon);
+                    } else {
+                        //Denne skal feile mykt, med en custom feilmelding til brukeren. Kaster dermed ingen feil her.
+                        Sentry.captureException(
+                            new Error('Klarte ikke sende inn søknaden', { cause: res.message })
+                        );
+                    }
                 }
-            }
-        );
+            );
 
-        settInnsendingStatus(res);
+            settInnsendingStatus(res);
 
-        return [res.status === RessursStatus.SUKSESS, formatert];
+            return [res.status === RessursStatus.SUKSESS, formatert];
+        } catch (error) {
+            throw new Error('Søknaden feilet på innsending', { cause: error });
+        }
     };
 
     return {
