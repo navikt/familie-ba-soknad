9dc18e1428c4327afbc3f77a2533bc63ef9b7918
Hanne Olsen <hanne.olsen@nav.no>
2024-09-03 11:04:58 +0200

    Fjerner toggle for ny kontoinformasjon, fjerner gammel KontonummerInfo for EØS-saker (#1341)

--- src/frontend/typer/feature-toggles.ts
+++ src/frontend/typer/feature-toggles.ts
@@ -5,14 +5,12 @@ export enum EKillSwitchToggle {
 export enum EFeatureToggle {
     // EKSEMPEL = 'EKSEMPEL',
     NYE_MODAL_TEKSTER = 'NYE_MODAL_TEKSTER',
-    VIS_KONTONUMMER = 'VIS_KONTONUMMER',
     BRUK_NYTT_ENDEPUNKT_FOR_INNSENDING_AV_SOKNAD = 'BRUK_NYTT_ENDEPUNKT_FOR_INNSENDING_AV_SOKNAD',
 }
 
 export const ToggleKeys: Record<EFeatureToggle, string> = {
     // [EFeatureToggle.EKSEMPEL]: 'familie-ba-soknad.eksempel',
     [EFeatureToggle.NYE_MODAL_TEKSTER]: 'familie-ba-soknad.nye-modal-tekster',
-    [EFeatureToggle.VIS_KONTONUMMER]: 'familie-ba-soknad.vis-kontonummer',
     [EFeatureToggle.BRUK_NYTT_ENDEPUNKT_FOR_INNSENDING_AV_SOKNAD]:
         'familie-ba-soknad.bruk_nytt_endepunkt_for_innsending_av_soknad',
 };
