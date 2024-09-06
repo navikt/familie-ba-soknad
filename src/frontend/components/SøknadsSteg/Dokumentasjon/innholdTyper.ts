682ae12ea32105a1731e9fbcd4ea849f2684d63b
bragejahren <brage.ekroll.jahren@nav.no>
2024-09-06 10:26:33 +0200

    Fikser feilet merge

--- src/frontend/components/SøknadsSteg/Dokumentasjon/innholdTyper.ts
+++ src/frontend/components/SøknadsSteg/Dokumentasjon/innholdTyper.ts
@@ -1,3 +1,4 @@
+import { BeskrivelseSanityApiNavn, TittelSanityApiNavn } from '../../../typer/dokumentasjon';
 import { LocaleRecordBlock, LocaleRecordString } from '../../../typer/sanity/sanity';
 
 export type IDokumentasjonTekstinnhold = {
@@ -5,14 +6,25 @@ export type IDokumentasjonTekstinnhold = {
     dokumentasjonGuide: LocaleRecordBlock;
     sendtInnTidligere: LocaleRecordBlock;
     vedleggXavY: LocaleRecordBlock;
-    dokumentasjonPaaSeparasjonSkilsmisseEllerDoedsfall: LocaleRecordBlock;
-    vedtakOmOppholdstillatelse: LocaleRecordBlock;
-    bekreftelsePaaAdopsjonBarnetrygd: LocaleRecordBlock;
-    bekreftelsePaaAtBarnBorSammenMedDeg: LocaleRecordBlock;
-    avtaleOmDeltBosted: LocaleRecordBlock;
-    meklingsattest: LocaleRecordBlock;
-    bekreftelseFraBarnevernetBarnetrygd: LocaleRecordBlock;
-    lastOppSenereISoknad: LocaleRecordBlock;
+} & {
+    [TittelSanityApiNavn.bekreftelsePaaAdopsjonTittel]: LocaleRecordBlock;
+    [TittelSanityApiNavn.annenDokumentasjon]: LocaleRecordBlock;
+    [TittelSanityApiNavn.avtaleOmDeltBostedTittel]: LocaleRecordBlock;
+    [TittelSanityApiNavn.bekreftelseFraBarnevernetTittel]: LocaleRecordBlock;
+    [TittelSanityApiNavn.bekreftelsePaaAtBarnBorSammenMedDegTittel]: LocaleRecordBlock;
+    [TittelSanityApiNavn.meklingsattestTittel]: LocaleRecordBlock;
+    [TittelSanityApiNavn.dokumentasjonPaaSeparasjonSkilsmisseEllerDoedsfallTittel]: LocaleRecordBlock;
+    [TittelSanityApiNavn.vedtakOmOppholdstillatelseTittel]: LocaleRecordBlock;
+} & {
+    [BeskrivelseSanityApiNavn.dokumentasjonPaaSeparasjonSkilsmisseEllerDoedsfall]: LocaleRecordBlock;
+    [BeskrivelseSanityApiNavn.vedtakOmOppholdstillatelse]: LocaleRecordBlock;
+    [BeskrivelseSanityApiNavn.bekreftelsePaaAdopsjonBarnetrygd]: LocaleRecordBlock;
+    [BeskrivelseSanityApiNavn.bekreftelsePaaAtBarnBorSammenMedDeg]: LocaleRecordBlock;
+    [BeskrivelseSanityApiNavn.avtaleOmDeltBosted]: LocaleRecordBlock;
+    [BeskrivelseSanityApiNavn.meklingsattest]: LocaleRecordBlock;
+    [BeskrivelseSanityApiNavn.bekreftelseFraBarnevernetBarnetrygd]: LocaleRecordBlock;
+    [BeskrivelseSanityApiNavn.lastOppSenereISoknad]: LocaleRecordBlock;
+} & {
     forMange: LocaleRecordString;
     feilFiltype: LocaleRecordString;
     forStor: LocaleRecordString;
