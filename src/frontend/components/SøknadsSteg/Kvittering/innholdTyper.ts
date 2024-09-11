226ed353e3f3f275bfc3274b222a69bebb147022
Ole Kristian Rudjord <89088327+ole-kristian-rudjord@users.noreply.github.com>
2024-09-11 10:03:17 +0200

    Forbedring av kvitteringssiden (#1360)
    * Endrer heading level fra xsmall til medium
    
    * Bytter tekster til å bruke Sanity og endrer tekstinnhold basert på om relevante vedlegg er innsendt eller ikke

--- src/frontend/components/SøknadsSteg/Kvittering/innholdTyper.ts
+++ src/frontend/components/SøknadsSteg/Kvittering/innholdTyper.ts
@@ -2,6 +2,10 @@ import { LocaleRecordBlock } from '../../../typer/sanity/sanity';
 
 export type IKvitteringTekstinnhold = {
     kvitteringTittel: LocaleRecordBlock;
+    soeknadMottatt: LocaleRecordBlock;
+    trengerIkkeEttersendeVedlegg: LocaleRecordBlock;
+    maaEttersendeVedleggAlert: LocaleRecordBlock;
+    infoTilSoker: LocaleRecordBlock;
     manglerKontonummerTittel: LocaleRecordBlock;
     kontonummerTittel: LocaleRecordBlock;
     redigerKontonummerLenke: LocaleRecordBlock;
