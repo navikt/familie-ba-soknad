d16ab59f54f01800d715a77fbda0300cac272c41
ole-kristian-rudjord <olekristianrudjord@gmail.com>
2024-09-17 16:28:54 +0200

    Endret layout og bruk av komponenter for PictureScanningGuide

--- src/frontend/components/Felleskomponenter/PictureScanningGuide/PictureScanningGuide.tsx
+++ src/frontend/components/Felleskomponenter/PictureScanningGuide/PictureScanningGuide.tsx
@@ -2,21 +2,16 @@ import React from 'react';
 
 import styled from 'styled-components';
 
-import { Heading, HGrid, VStack } from '@navikt/ds-react';
+import { ExpansionCard, Heading, HGrid, VStack } from '@navikt/ds-react';
 
 import { useApp } from '../../../context/AppContext';
-import ExpandableInfo from '../expandableContent/ExpandableInfo';
 import TekstBlock from '../Sanity/TekstBlock';
 
 import PictureScanningExample from './PictureScanningExample';
 import ScanningIcon from './ScanningIcon';
 
-const Container = styled.div`
-    svg {
-        pointer-events: none;
-    }
-
-    ul {
+const StyledVStack = styled(VStack)`
+    && ul {
         margin: 0;
         li {
             margin-bottom: 0.5rem;
@@ -28,10 +23,19 @@ const PictureScanningGuide = () => {
     const { tekster, plainTekst } = useApp();
     const dokumentasjonTekster = tekster().DOKUMENTASJON;
     const svgIconHeight = 100;
+
     return (
-        <ExpandableInfo title={plainTekst(dokumentasjonTekster.slikTarDuEtGodtBildeExpand)}>
-            <Container>
-                <VStack gap="8">
+        <ExpansionCard
+            size="small"
+            aria-label={plainTekst(dokumentasjonTekster.slikTarDuEtGodtBildeExpand)}
+        >
+            <ExpansionCard.Header>
+                <ExpansionCard.Title as="h3" size="medium">
+                    {plainTekst(dokumentasjonTekster.slikTarDuEtGodtBildeExpand)}
+                </ExpansionCard.Title>
+            </ExpansionCard.Header>
+            <ExpansionCard.Content>
+                <StyledVStack gap="8">
                     <div>
                         <Heading level="4" size="xsmall" spacing>
                             {plainTekst(dokumentasjonTekster.slikTarDuEtGodtBildeTittel)}
@@ -48,31 +52,28 @@ const PictureScanningGuide = () => {
                         <Heading level="4" size="xsmall" spacing>
                             {plainTekst(dokumentasjonTekster.braOgDaarligeTittel)}
                         </Heading>
-                        <HGrid gap="6 4" marginBlock="6 0" columns={{ xs: 2, md: 4 }}>
+                        <HGrid gap="4" columns={{ xs: 1, sm: 2 }}>
                             <PictureScanningExample
                                 image={<ScanningIcon status="good" height={svgIconHeight} />}
-                                status="suksess"
+                                variant="success"
                                 statusText={plainTekst(dokumentasjonTekster.bra)}
                                 description={plainTekst(dokumentasjonTekster.fyllerHeleBildet)}
                             />
-
                             <PictureScanningExample
                                 image={<ScanningIcon status="keystone" height={svgIconHeight} />}
-                                status="feil"
+                                variant="error"
                                 statusText={plainTekst(dokumentasjonTekster.daarlig)}
                                 description={plainTekst(dokumentasjonTekster.ikkeTattOvenfra)}
                             />
-
                             <PictureScanningExample
                                 image={<ScanningIcon status="horizontal" height={svgIconHeight} />}
-                                status="feil"
+                                variant="error"
                                 statusText={plainTekst(dokumentasjonTekster.daarlig)}
                                 description={plainTekst(dokumentasjonTekster.ikkeRiktigRetning)}
                             />
-
                             <PictureScanningExample
                                 image={<ScanningIcon status="shadow" height={svgIconHeight} />}
-                                status="feil"
+                                variant="error"
                                 statusText={plainTekst(dokumentasjonTekster.daarlig)}
                                 description={plainTekst(dokumentasjonTekster.skyggePaaDokumentet)}
                             />
@@ -84,9 +85,9 @@ const PictureScanningGuide = () => {
                         </Heading>
                         <TekstBlock block={dokumentasjonTekster.vaerTryggNaarDuTarBilde} />
                     </div>
-                </VStack>
-            </Container>
-        </ExpandableInfo>
+                </StyledVStack>
+            </ExpansionCard.Content>
+        </ExpansionCard>
     );
 };
 export default PictureScanningGuide;
