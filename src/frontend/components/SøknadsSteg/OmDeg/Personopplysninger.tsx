90a20e26c64d98bc5cb26c509cec1679056493a0
ole-kristian-rudjord <olekristianrudjord@gmail.com>
2024-10-24 13:36:51 +0200

    Endrer fra bruk av Label til BodyShort

--- src/frontend/components/SøknadsSteg/OmDeg/Personopplysninger.tsx
+++ src/frontend/components/SøknadsSteg/OmDeg/Personopplysninger.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Alpha3Code } from 'i18n-iso-countries';
 
-import { BodyShort, Label } from '@navikt/ds-react';
+import { BodyShort } from '@navikt/ds-react';
 
 import { useApp } from '../../../context/AppContext';
 import { useSpråk } from '../../../context/SpråkContext';
@@ -34,19 +34,19 @@ export const Personopplysninger: React.FC = () => {
     return (
         <>
             <div>
-                <Label>{plainTekst(ident)}</Label>
+                <BodyShort weight="semibold">{plainTekst(ident)}</BodyShort>
                 <BodyShort>{søker.ident}</BodyShort>
             </div>
             <div>
-                <Label>{plainTekst(statsborgerskap)}</Label>
+                <BodyShort weight="semibold">{plainTekst(statsborgerskap)}</BodyShort>
                 <BodyShort>{statsborgerskapTekst}</BodyShort>
             </div>
             <div>
-                <Label>{plainTekst(sivilstatus)}</Label>
+                <BodyShort weight="semibold">{plainTekst(sivilstatus)}</BodyShort>
                 <BodyShort>{sivilStatusTekst}</BodyShort>
             </div>
             <div>
-                <Label>{plainTekst(adresse)}</Label>
+                <BodyShort weight="semibold">{plainTekst(adresse)}</BodyShort>
                 {genererAdresseVisning(søker, tekster().OM_DEG, plainTekst)}
             </div>
         </>
