e2fb98b1d2a8a3df0e656a0581971b8329ca9c12
Hanne Olsen <hanne.olsen@nav.no>
2024-11-15 15:06:33 +0100

    Fikser sentry-feil med contexter som ikke er tilgjengelige på feilsiden

--- src/frontend/components/Felleskomponenter/Feilside/LastInnSidenPåNyttLenke.tsx
+++ src/frontend/components/Felleskomponenter/Feilside/LastInnSidenPåNyttLenke.tsx
@@ -2,23 +2,13 @@ import React, { FC, ReactNode } from 'react';
 
 import { Link } from '@navikt/ds-react';
 
-import { useSteg } from '../../../context/StegContext';
-
 interface ILastInnSidenPåNyttLenkeProps {
     children: ReactNode;
 }
 
 export const LastInnSidenPåNyttLenke: FC<ILastInnSidenPåNyttLenkeProps> = ({ children }) => {
-    const { hentNåværendeSteg } = useSteg();
-    const nåværendeSteg = hentNåværendeSteg();
-
-    const håndterLastInnPåNytt = (event: React.MouseEvent) => {
-        event.preventDefault();
-        location.reload();
-    };
-
     return (
-        <Link href={nåværendeSteg.path} variant="action" onClick={håndterLastInnPåNytt}>
+        <Link href={location.href} variant="action">
             {children}
         </Link>
     );
