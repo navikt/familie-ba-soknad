bbb27f7e4581cd645efb426ca36eed901b6cfca8
Ida Merete Enholm <ida.merete.enholm@nav.no>
2024-11-21 14:04:55 +0100

    bytt ut .eslintrc med eslint.config.mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,125 @@
+import eslintConfigPrettier from 'eslint-config-prettier';
+import prettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import globals from 'globals';
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+    js.configs.recommended,
+    eslintConfigPrettier,
+    importPlugin.flatConfigs.typescript,
+    ...tseslint.configs.recommended,
+    {
+        plugins: {
+            '@typescript-eslint': tseslint.plugin,
+            prettier: prettier,
+            import: importPlugin,
+        },
+
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+
+            parser: tseslint.parser,
+            ecmaVersion: 2020,
+            sourceType: 'module',
+
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+
+        rules: {
+            'no-case-declarations': 'off',
+
+            'import/extensions': [
+                'off',
+                'ignorePackages',
+                {
+                    js: 'never',
+                    jsx: 'never',
+                    ts: 'never',
+                    tsx: 'never',
+                },
+            ],
+
+            'import/no-cycle': 1,
+            '@typescript-eslint/ban-ts-ignore': 'off',
+            '@typescript-eslint/explicit-module-boundary-types': 'off',
+            'prettier/prettier': 'error',
+            '@typescript-eslint/interface-name-prefix': 'off',
+            '@typescript-eslint/no-var-requires': 'warn',
+            '@typescript-eslint/explicit-function-return-type': 'off',
+
+            '@typescript-eslint/no-unused-vars': [
+                'warn',
+                {
+                    vars: 'all',
+                    args: 'all',
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                },
+            ],
+
+            '@typescript-eslint/ban-types': [
+                'error',
+                {
+                    types: {
+                        object: false,
+                    },
+
+                    extendDefaults: true,
+                },
+            ],
+
+            'import/named': 'error',
+            'import/namespace': 'error',
+            'import/default': 'error',
+            'import/export': 'error',
+
+            'import/order': [
+                'error',
+                {
+                    alphabetize: {
+                        order: 'asc',
+                        caseInsensitive: true,
+                    },
+
+                    'newlines-between': 'always',
+                    groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+
+                    pathGroups: [
+                        {
+                            pattern: 'react',
+                            group: 'external',
+                            position: 'before',
+                        },
+                        {
+                            pattern: 'nav-**',
+                            group: 'external',
+                            position: 'after',
+                        },
+                        {
+                            pattern: '@navikt/**',
+                            group: 'internal',
+                            position: 'before',
+                        },
+                    ],
+
+                    pathGroupsExcludedImportTypes: ['builtin'],
+                },
+            ],
+        },
+    },
+];
