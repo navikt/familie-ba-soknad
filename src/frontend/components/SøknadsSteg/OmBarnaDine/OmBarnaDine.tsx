be2432310ef741b6ad2db71d5fac78852f29e734
Hanne Olsen <hanne.olsen@nav.no>
2025-01-14 15:45:24 +0100

    Erstatter alle relevante imports

--- src/frontend/components/SøknadsSteg/OmBarnaDine/OmBarnaDine.tsx
+++ src/frontend/components/SøknadsSteg/OmBarnaDine/OmBarnaDine.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router';
 
 import { ESvar } from '@navikt/familie-form-elements';
 
