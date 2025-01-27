602448c9c5d5e79f3d7ba75d3052ef2fb53c1e9a
ole-kristian-rudjord <olekristianrudjord@gmail.com>
2025-01-27 16:01:11 +0100

    Bytter til bruk av Sanity tekster i pensjonsperioder.ts

--- src/frontend/typer/sanity/modaler/pensjonsperiode.ts
+++ src/frontend/typer/sanity/modaler/pensjonsperiode.ts
@@ -1,9 +1,15 @@
-import { LocaleRecordBlock, LocaleRecordString } from '../sanity';
+import { ISanitySpørsmålDokument, LocaleRecordBlock, LocaleRecordString } from '../sanity';
 
 export interface IPensjonsperiodeTekstinnhold {
     tittel: LocaleRecordBlock;
+    oppsummeringstittel: LocaleRecordBlock;
     leggTilPeriodeForklaring: LocaleRecordString;
     flerePerioder: LocaleRecordString;
     leggTilKnapp: LocaleRecordBlock;
     leggTilFeilmelding: LocaleRecordBlock;
+    faarPensjonNaa: ISanitySpørsmålDokument;
+    pensjonLandFortid: ISanitySpørsmålDokument;
+    pensjonLandNaatid: ISanitySpørsmålDokument;
+    startdato: ISanitySpørsmålDokument;
+    sluttdato: ISanitySpørsmålDokument;
 }
--- src/frontend/utils/mappingTilKontrakt/pensjonsperioder.ts
+++ src/frontend/utils/mappingTilKontrakt/pensjonsperioder.ts
@@ -1,15 +1,12 @@
 import { ESvar } from '@navikt/familie-form-elements';
 
-import {
-    pensjonsperiodeModalSpørsmålSpråkId,
-    pensjonsperiodeOppsummeringOverskrift,
-} from '../../components/Felleskomponenter/Pensjonsmodal/språkUtils';
-import { PensjonsperiodeSpørsmålId } from '../../components/Felleskomponenter/Pensjonsmodal/spørsmål';
-import { ISøknadsfelt } from '../../typer/kontrakt/generelle';
+import { IBarnMedISøknad } from '../../typer/barn';
+import { ISøknadsfelt, TilRestLocaleRecord } from '../../typer/kontrakt/generelle';
 import { IPensjonsperiodeIKontraktFormat } from '../../typer/kontrakt/kontrakt';
 import { IPensjonsperiode } from '../../typer/perioder';
 import { PeriodePersonTypeMedBarnProps, PersonType } from '../../typer/personType';
-import { hentTekster, landkodeTilSpråk } from '../språk';
+import { IPensjonsperiodeTekstinnhold } from '../../typer/sanity/modaler/pensjonsperiode';
+import { landkodeTilSpråk } from '../språk';
 
 import { sammeVerdiAlleSpråk, verdiCallbackAlleSpråk } from './hjelpefunksjoner';
 
@@ -17,15 +14,20 @@ interface PensjonsperiodeIKontraktFormatParams {
     periode: IPensjonsperiode;
     periodeNummer: number;
     gjelderUtlandet: boolean;
+    tilRestLocaleRecord: TilRestLocaleRecord;
+    tekster: IPensjonsperiodeTekstinnhold;
+    barn?: IBarnMedISøknad;
 }
 
 export const tilIPensjonsperiodeIKontraktFormat = ({
     periode,
     periodeNummer,
     gjelderUtlandet,
+    tilRestLocaleRecord,
+    tekster,
+    barn,
     personType,
     erDød,
-    barn,
 }: PensjonsperiodeIKontraktFormatParams &
     PeriodePersonTypeMedBarnProps): ISøknadsfelt<IPensjonsperiodeIKontraktFormat> => {
     const { mottarPensjonNå, pensjonsland, pensjonFra, pensjonTil } = periode;
@@ -33,30 +35,30 @@ export const tilIPensjonsperiodeIKontraktFormat = ({
     const periodenErAvsluttet =
         mottarPensjonNå?.svar === ESvar.NEI || (personType === PersonType.AndreForelder && !!erDød);
 
-    const hentPensjonsperiodeSpråkId = pensjonsperiodeModalSpørsmålSpråkId(
-        personType,
-        periodenErAvsluttet
-    );
-
-    const hentSpørsmålstekster = (pensjonSpørsmålId: PensjonsperiodeSpørsmålId) =>
-        hentTekster(hentPensjonsperiodeSpråkId(pensjonSpørsmålId), {
-            ...(barn && { barn: barn.navn }),
-        });
-
     return {
-        label: hentTekster(pensjonsperiodeOppsummeringOverskrift(gjelderUtlandet), {
-            x: periodeNummer,
+        label: tilRestLocaleRecord(tekster.oppsummeringstittel, {
+            antall: periodeNummer.toString(),
+            gjelderUtland: gjelderUtlandet,
         }),
         verdi: sammeVerdiAlleSpråk({
             mottarPensjonNå: mottarPensjonNå.svar
                 ? {
-                      label: hentSpørsmålstekster(PensjonsperiodeSpørsmålId.mottarPensjonNå),
+                      label: tilRestLocaleRecord(tekster.faarPensjonNaa.sporsmal, {
+                          barnetsNavn: barn?.navn,
+                      }),
                       verdi: sammeVerdiAlleSpråk(mottarPensjonNå.svar),
                   }
                 : null,
             pensjonsland: pensjonsland.svar
                 ? {
-                      label: hentSpørsmålstekster(PensjonsperiodeSpørsmålId.pensjonsland),
+                      label: tilRestLocaleRecord(
+                          periodenErAvsluttet
+                              ? tekster.pensjonLandFortid.sporsmal
+                              : tekster.pensjonLandNaatid.sporsmal,
+                          {
+                              barnetsNavn: barn?.navn,
+                          }
+                      ),
                       verdi: verdiCallbackAlleSpråk(locale =>
                           landkodeTilSpråk(pensjonsland.svar, locale)
                       ),
@@ -64,13 +66,13 @@ export const tilIPensjonsperiodeIKontraktFormat = ({
                 : null,
             pensjonFra: pensjonFra.svar
                 ? {
-                      label: hentSpørsmålstekster(PensjonsperiodeSpørsmålId.fraDatoPensjon),
+                      label: tilRestLocaleRecord(tekster.startdato.sporsmal),
                       verdi: sammeVerdiAlleSpråk(pensjonFra.svar),
                   }
                 : null,
             pensjonTil: pensjonTil.svar
                 ? {
-                      label: hentSpørsmålstekster(PensjonsperiodeSpørsmålId.tilDatoPensjon),
+                      label: tilRestLocaleRecord(tekster.sluttdato.sporsmal),
                       verdi: sammeVerdiAlleSpråk(pensjonTil.svar),
                   }
                 : null,
