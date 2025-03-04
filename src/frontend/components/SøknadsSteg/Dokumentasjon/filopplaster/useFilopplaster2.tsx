f9d2383c2c5f2040778b98fbfd7e68dd481d5fb8
ole-kristian-rudjord <olekristianrudjord@gmail.com>
2025-03-04 14:49:51 +0100

    Forbedrer lesbarhet

--- src/frontend/components/SøknadsSteg/Dokumentasjon/filopplaster/useFilopplaster2.tsx
+++ src/frontend/components/SøknadsSteg/Dokumentasjon/filopplaster/useFilopplaster2.tsx
@@ -27,10 +27,40 @@ enum EStandardFileRejectionReasons {
 }
 
 enum ECustomFileRejectionReasons {
-    FIL_STØRRELSE_FOR_LITEN = 'filStørrelseForLiten',
+    FIL_DIMENSJONER_FOR_LITEN = 'filDimensjonerForLiten',
     MAKS_ANTALL_FILER_NÅDD = 'maksAntallFilerNådd',
     NOE_GIKK_FEIL = 'noeGikkFeil',
 }
+
+const filtrerFiler = (
+    filer: FileObject[],
+    antallEksisterendeFiler: number,
+    maksAntallFiler: number
+): { aksepterteFiler: FileAccepted[]; feilendeFiler: FileRejected[] } => {
+    const filerUtenFeil: FileAccepted[] = filer.filter(file => !file.error);
+    const filerMedFeil: FileRejected[] = filer.filter(file => file.error);
+
+    const forMangeFiler = antallEksisterendeFiler + filerUtenFeil.length > maksAntallFiler;
+
+    if (!forMangeFiler) {
+        return { aksepterteFiler: filerUtenFeil, feilendeFiler: filerMedFeil };
+    } else {
+        const ledigePlasser = maksAntallFiler - antallEksisterendeFiler;
+
+        const aksepterteFiler = filerUtenFeil.slice(0, ledigePlasser);
+
+        const filerOverMaksAntall = filerUtenFeil.slice(aksepterteFiler.length);
+        const filerOverMaksAntallMedFeil: FileRejected[] = filerOverMaksAntall.map(fil => ({
+            file: fil.file,
+            error: true,
+            reasons: [ECustomFileRejectionReasons.MAKS_ANTALL_FILER_NÅDD],
+        }));
+
+        const feilendeFiler = [...filerMedFeil, ...filerOverMaksAntallMedFeil];
+
+        return { aksepterteFiler, feilendeFiler };
+    }
+};
 
 enum BadRequestCode {
     IMAGE_TOO_LARGE = 'IMAGE_TOO_LARGE',
@@ -51,7 +81,7 @@ const feilmeldingFraError = (error: AxiosError): string => {
     const badRequestCode = badRequestCodeFraError(error);
     switch (badRequestCode) {
         case BadRequestCode.IMAGE_DIMENSIONS_TOO_SMALL:
-            return ECustomFileRejectionReasons.FIL_STØRRELSE_FOR_LITEN;
+            return ECustomFileRejectionReasons.FIL_DIMENSJONER_FOR_LITEN;
         case BadRequestCode.IMAGE_TOO_LARGE:
             return EStandardFileRejectionReasons.FIL_STØRRELSE_FOR_STOR;
         case BadRequestCode.INVALID_DOCUMENT_FORMAT:
@@ -85,7 +115,7 @@ export const useFilopplaster2 = (
             dokumentasjonTekster.filtypeFeilmelding
         ),
         [EStandardFileRejectionReasons.FIL_STØRRELSE_FOR_STOR]: `${plainTekst(dokumentasjonTekster.filstorrelseFeilmelding)} ${MAKS_FILSTØRRELSE_MB} MB`,
-        [ECustomFileRejectionReasons.FIL_STØRRELSE_FOR_LITEN]: plainTekst(
+        [ECustomFileRejectionReasons.FIL_DIMENSJONER_FOR_LITEN]: plainTekst(
             dokumentasjonTekster.bildetForLite
         ),
         [ECustomFileRejectionReasons.MAKS_ANTALL_FILER_NÅDD]: `${plainTekst(dokumentasjonTekster.antallFilerFeilmelding)} ${MAKS_ANTALL_FILER}`,
@@ -95,30 +125,21 @@ export const useFilopplaster2 = (
     const dagensDatoStreng = new Date().toISOString();
 
     const leggTilVedlegg = async (nyeFiler: FileObject[]) => {
-        const ledigePlasser = MAKS_ANTALL_FILER - dokumentasjon.opplastedeVedlegg.length;
+        const { aksepterteFiler, feilendeFiler } = filtrerFiler(
+            nyeFiler,
+            dokumentasjon.opplastedeVedlegg.length,
+            MAKS_ANTALL_FILER
+        );
 
-        const gyldigeFiler: FileAccepted[] = nyeFiler.filter(file => !file.error);
-        const feilendeFiler: FileRejected[] = nyeFiler.filter(file => file.error);
-
-        const aksepterteFiler = gyldigeFiler.slice(0, ledigePlasser);
-
-        const filerOverMaksAntall = gyldigeFiler.slice(aksepterteFiler.length);
-        const filerOverMaksAntallMedFeilmelding: FileRejected[] = filerOverMaksAntall.map(fil => {
-            return {
-                file: fil.file,
-                error: true,
-                reasons: [ECustomFileRejectionReasons.MAKS_ANTALL_FILER_NÅDD],
-            };
-        });
-
-        const nyeAvvisteFiler = [...feilendeFiler, ...filerOverMaksAntallMedFeilmelding];
-
-        setAvvsiteFiler([...avvisteFiler, ...nyeAvvisteFiler]);
+        if (feilendeFiler.length > 0) {
+            setAvvsiteFiler([...avvisteFiler, ...feilendeFiler]);
+        }
 
         const nyeVedlegg: IVedlegg[] = [];
+        const avvisteVedlegg: FileRejected[] = [];
 
         await Promise.all(
-            aksepterteFiler.map(fil => {
+            aksepterteFiler.map((fil: FileAccepted) => {
                 return wrapMedSystemetLaster(async () => {
                     const requestData = new FormData();
                     requestData.append('file', fil.file);
@@ -137,21 +158,21 @@ export const useFilopplaster2 = (
                         )
                         .then((response: { data: OpplastetVedlegg }) => {
                             const { data } = response;
-                            nyeVedlegg.push({
+                            const nyttVedlegg: IVedlegg = {
                                 dokumentId: data.dokumentId,
                                 navn: fil.file.name,
                                 størrelse: fil.file.size,
                                 tidspunkt: dagensDatoStreng,
-                            });
+                            };
+                            nyeVedlegg.push(nyttVedlegg);
                         })
                         .catch((error: AxiosError) => {
-                            const oppfangetFeil: FileRejected = {
+                            const filMedFeil: FileRejected = {
                                 file: fil.file,
                                 error: true,
                                 reasons: [feilmeldingFraError(error)],
                             };
-
-                            setAvvsiteFiler([...avvisteFiler, oppfangetFeil]);
+                            avvisteVedlegg.push(filMedFeil);
                         });
                 });
             })
@@ -164,13 +185,16 @@ export const useFilopplaster2 = (
                 dokumentasjon.harSendtInn
             );
         }
+
+        if (avvisteVedlegg.length > 0) {
+            setAvvsiteFiler([...avvisteFiler, ...avvisteVedlegg]);
+        }
     };
 
     const fjernVedlegg = (vedleggSomSkalFjernes: IVedlegg) => {
         const nyVedleggsliste = dokumentasjon.opplastedeVedlegg.filter(
-            (opplastetVedlegg: IVedlegg) => {
-                return opplastetVedlegg.dokumentId !== vedleggSomSkalFjernes.dokumentId;
-            }
+            (eksisterendeVedlegg: IVedlegg) =>
+                eksisterendeVedlegg.dokumentId !== vedleggSomSkalFjernes.dokumentId
         );
 
         oppdaterDokumentasjon(
