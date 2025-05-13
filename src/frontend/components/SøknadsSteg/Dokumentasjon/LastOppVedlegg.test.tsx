d253b70355b3dfb4728a43013de0e533796b137f
ole-kristian-rudjord <olekristianrudjord@gmail.com>
2025-05-13 14:33:52 +0200

    Legger tilbake testing

--- src/frontend/components/SøknadsSteg/Dokumentasjon/LastOppVedlegg.test.tsx
+++ src/frontend/components/SøknadsSteg/Dokumentasjon/LastOppVedlegg.test.tsx
@@ -3,25 +3,27 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { mockDeep } from 'jest-mock-extended';
 
-// import { IDokumentasjon } from '../../../typer/dokumentasjon';
-// import { Dokumentasjonsbehov } from '../../../typer/kontrakt/dokumentasjon';
+import { IDokumentasjon } from '../../../typer/dokumentasjon';
+import { Dokumentasjonsbehov } from '../../../typer/kontrakt/dokumentasjon';
 import { ESivilstand, ESøknadstype } from '../../../typer/kontrakt/generelle';
 import { ISøker } from '../../../typer/person';
 import { mockEøs, silenceConsoleErrors, spyOnUseApp, TestProvidere } from '../../../utils/testing';
 
-// const hentAnnenDokumentasjon = (): IDokumentasjon => {
-//     jest.resetModules();
-//     const { initialStateSøknad } = jest.requireActual('../../../typer/søknad');
+import LastOppVedlegg from './LastOppVedlegg';
 
-//     const dokumentasjon = initialStateSøknad(false).dokumentasjon.find(
-//         dok => dok.dokumentasjonsbehov === Dokumentasjonsbehov.ANNEN_DOKUMENTASJON
-//     );
+const hentAnnenDokumentasjon = (): IDokumentasjon => {
+    jest.resetModules();
+    const { initialStateSøknad } = jest.requireActual('../../../typer/søknad');
 
-//     if (dokumentasjon === undefined) {
-//         throw new Error('Fant ikke dokumentasjonsbehov ANNEN_DOKUMENTASJON');
-//     }
-//     return dokumentasjon;
-// };
+    const dokumentasjon = initialStateSøknad(false).dokumentasjon.find(
+        dok => dok.dokumentasjonsbehov === Dokumentasjonsbehov.ANNEN_DOKUMENTASJON
+    );
+
+    if (dokumentasjon === undefined) {
+        throw new Error('Fant ikke dokumentasjonsbehov ANNEN_DOKUMENTASJON');
+    }
+    return dokumentasjon;
+};
 
 describe('LastOppVedlegg', () => {
     beforeEach(() => {
@@ -32,15 +34,15 @@ describe('LastOppVedlegg', () => {
 
     it('Viser ikke info-tekst og checkbox knapp for ANNEN_DOKUMENTASJON', () => {
         spyOnUseApp({});
-        // const dokumentasjon = hentAnnenDokumentasjon();
-        // const oppdaterDokumentasjon = jest.fn();
+        const dokumentasjon = hentAnnenDokumentasjon();
+        const oppdaterDokumentasjon = jest.fn();
 
         const { getByTestId, queryByTestId } = render(
             <TestProvidere>
-                {/* <LastOppVedlegg
+                <LastOppVedlegg
                     dokumentasjon={dokumentasjon}
                     oppdaterDokumentasjon={oppdaterDokumentasjon}
-                /> */}
+                />
             </TestProvidere>
         );
 
@@ -63,15 +65,15 @@ describe('LastOppVedlegg', () => {
         });
 
         return import('../../../typer/søknad').then(() => {
-            // const dokumentasjon = hentAnnenDokumentasjon();
-            // const oppdaterDokumentasjon = jest.fn();
+            const dokumentasjon = hentAnnenDokumentasjon();
+            const oppdaterDokumentasjon = jest.fn();
 
             const { getByTestId, queryByTestId } = render(
                 <TestProvidere>
-                    {/* <LastOppVedlegg
+                    <LastOppVedlegg
                         dokumentasjon={dokumentasjon}
                         oppdaterDokumentasjon={oppdaterDokumentasjon}
-                    /> */}
+                    />
                 </TestProvidere>
             );
 
