be328725b9d5a52f4bff7919f9baac190971fbdb
Hanne Olsen <hanne.olsen@nav.no>
2025-06-23 14:40:49 +0200

    Installerer MSW og fikser default config

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,7 @@
 import '@testing-library/jest-dom/vitest';
+
+import { server } from './mocks/node.js';
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
