f38a855e963adf1b50c34d96c5f6979a6a9ab340
Hanne Olsen <hanne.olsen@nav.no>
2025-06-23 15:34:53 +0200

    Legger på riktige handlere for API-requestene våre

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,11 +1,37 @@
 import { http, HttpResponse } from 'msw';
 
+import { byggSuksessRessurs } from '@navikt/familie-typer';
+
 export const handlers = [
-    http.get('https://api.example.com/user', () => {
-        return HttpResponse.json({
-            id: 'abc-123',
-            firstName: 'John',
-            lastName: 'Maverick',
-        });
+    http.get('/modellversjon', () => {
+        return HttpResponse.json(byggSuksessRessurs(50));
+    }),
+    http.get('/toggles/all', () => {
+        return HttpResponse.json(byggSuksessRessurs('toggle1'));
+    }),
+    http.get('api/innlogget/barnetrygd', () => {
+        return HttpResponse.json(byggSuksessRessurs('Autentisert kall'));
+    }),
+
+    http.get('/dokument/soknad/barnetrygd', () => {
+        return new HttpResponse(null, { status: 404 });
+    }),
+    http.get('/api/kontoregister/hent-kontonr', () => {
+        return HttpResponse.json(
+            byggSuksessRessurs({
+                kontonummer: '815.493.00',
+            })
+        );
+    }),
+    http.get('/api/kodeverk/eos-land', () => {
+        return HttpResponse.json(
+            byggSuksessRessurs({
+                BEL: 'Belgia',
+                DNK: 'Danmark',
+                CHE: 'Sveits',
+                IRL: 'Irland',
+                NLD: 'Nederland',
+            })
+        );
     }),
 ];
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,7 +1,12 @@
 import '@testing-library/jest-dom/vitest';
+import { afterEach, vi } from 'vitest';
 
 import { server } from './mocks/node.js';
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
+
+// Dekoratøren feiler på grunn av timeouts i CSP-oppsettet.
+// Den er ikke relevant for testene våre, fjerner den dermed
+vi.mock('src/frontend/decorator.ts');
