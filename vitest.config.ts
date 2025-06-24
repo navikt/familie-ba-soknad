ade85f9bd858cb4a8021b6816f62fe8614586848
Hanne Olsen <hanne.olsen@nav.no>
2025-06-24 10:46:39 +0200

    Fjerner jest

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,4 @@ export default defineConfig({
         environment: 'jsdom',
         setupFiles: './vitest.setup.ts',
     },
-    // For å fikse window-problemer med dekoratøren:
-    // https://github.com/vitest-dev/vitest/issues/1293#issuecomment-1560660670
-    define: process.env.VITEST ? {} : { global: 'window' },
 });
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,8 +1,14 @@
 import '@testing-library/jest-dom/vitest';
 import { afterEach } from 'vitest';
 
+import * as decorator from '@navikt/nav-dekoratoren-moduler';
+
 import { server } from './mocks/node.js';
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
+
+window.scrollTo = () => {
+    // Ikke implementert
+};
