13c32076b4204dbed1f47f7bebdb7bf0f6edb487
Hanne Olsen <hanne.olsen@nav.no>
2025-08-29 13:38:30 +0200

    Fjerner ubrukte funksjoner for språkID, den gamle måten å sette tekster på

--- src/frontend/components/Felleskomponenter/UtenlandsoppholdModal/utenlandsoppholdSpråkUtils.ts
+++ src/frontend/components/Felleskomponenter/UtenlandsoppholdModal/utenlandsoppholdSpråkUtils.ts
@@ -3,13 +3,7 @@ import { IUtenlandsoppholdTekstinnhold } from '../../../typer/sanity/modaler/ute
 import { LocaleRecordBlock, LocaleRecordString } from '../../../typer/sanity/sanity';
 import { EUtenlandsoppholdÅrsak } from '../../../typer/utenlandsopphold';
 
-import {
-    landFeilmeldingSpråkIdsSøker,
-    landLabelSpråkIdsBarn,
-    landLabelSpråkIdsSøker,
-    årsakSpråkIdsBarn,
-    årsakSpråkIdsSøker,
-} from './spørsmål';
+import { landFeilmeldingSpråkIdsSøker } from './spørsmål';
 
 export const hentUtenlandsoppholdÅrsak = (
     årsak: EUtenlandsoppholdÅrsak | '',
@@ -111,24 +105,12 @@ export const hentTilDatoSpørsmål = (
     }
 };
 
-export const årsakFeilmeldingSpråkId = (barn?: IBarnMedISøknad): string =>
-    barn ? 'ombarnet.beskriveopphold.feilmelding' : 'modal.beskriveopphold.feilmelding';
-
-export const årsakLabelSpråkId = (barn?: IBarnMedISøknad): string =>
-    barn ? 'ombarnet.beskriveopphold.spm' : 'modal.beskriveopphold.spm';
-
-export const årsakSpråkId = (årsak: EUtenlandsoppholdÅrsak | '', barn?: IBarnMedISøknad): string =>
-    barn ? årsakSpråkIdsBarn[årsak] : årsakSpråkIdsSøker[årsak];
-
-export const landLabelSpråkId = (årsak: EUtenlandsoppholdÅrsak | '', barn?: IBarnMedISøknad) =>
-    barn ? landLabelSpråkIdsBarn[årsak] : landLabelSpråkIdsSøker[årsak];
-
 export const landFeilmeldingSpråkId = (
     årsak: EUtenlandsoppholdÅrsak | '',
     barn?: IBarnMedISøknad
 ) => (barn ? landFeilmeldingSpråkIdsBarn[årsak] : landFeilmeldingSpråkIdsSøker[årsak]);
 
-export const landFeilmeldingSpråkIdsBarn: Record<EUtenlandsoppholdÅrsak, string> = {
+const landFeilmeldingSpråkIdsBarn: Record<EUtenlandsoppholdÅrsak, string> = {
     [EUtenlandsoppholdÅrsak.FLYTTET_PERMANENT_TIL_NORGE]:
         'ombarnet.hvilketlandflyttetfra.feilmelding',
     [EUtenlandsoppholdÅrsak.FLYTTET_PERMANENT_FRA_NORGE]:
@@ -150,41 +132,5 @@ export const fraDatoFeilmeldingSpråkId = (
                 : 'modal.nårflyttetfra.feilmelding';
         default:
             return 'felles.nårstartetoppholdet.feilmelding';
-    }
-};
-
-export const fraDatoLabelSpråkId = (årsak: EUtenlandsoppholdÅrsak | '', barn?: IBarnMedISøknad) => {
-    switch (årsak) {
-        case EUtenlandsoppholdÅrsak.FLYTTET_PERMANENT_FRA_NORGE:
-            return barn ? 'ombarnet.nårflyttetfranorge.spm' : 'modal.nårflyttetfra.spm';
-        default:
-            return 'felles.nårstartetoppholdet.spm';
-    }
-};
-
-export const tilDatoLabelSpråkId = (årsak: EUtenlandsoppholdÅrsak | '', barn?: IBarnMedISøknad) => {
-    switch (årsak) {
-        case EUtenlandsoppholdÅrsak.FLYTTET_PERMANENT_TIL_NORGE:
-            return barn ? 'ombarnet.nårflyttettilnorge.spm' : 'modal.nårflyttettilnorge.spm';
-        case EUtenlandsoppholdÅrsak.HAR_OPPHOLDT_SEG_UTENFOR_NORGE:
-            return 'felles.nåravsluttetoppholdet.spm';
-        default:
-            return 'felles.nåravsluttesoppholdet.spm';
-    }
-};
-
-export const tilDatoFeilmeldingSpråkId = (
-    årsak: EUtenlandsoppholdÅrsak | '',
-    barn?: IBarnMedISøknad
-) => {
-    switch (årsak) {
-        case EUtenlandsoppholdÅrsak.FLYTTET_PERMANENT_TIL_NORGE:
-            return barn
-                ? 'ombarnet.nårflyttettilnorge.feilmelding'
-                : 'modal.nårflyttettilnorge.feilmelding';
-        case EUtenlandsoppholdÅrsak.HAR_OPPHOLDT_SEG_UTENFOR_NORGE:
-            return 'felles.nåravsluttetoppholdet.feilmelding';
-        default:
-            return 'felles.nåravsluttesoppholdet.feilmelding';
     }
 };
--- src/frontend/typer/dokumentasjon.ts
+++ src/frontend/typer/dokumentasjon.ts
@@ -22,27 +22,6 @@ export enum EFiltyper {
     JPEG = '.jpeg',
 }
 
-export const dokumentasjonsbehovTilSpråkId = (dokumentasjonsbehov: Dokumentasjonsbehov): string => {
-    switch (dokumentasjonsbehov) {
-        case Dokumentasjonsbehov.ADOPSJON_DATO:
-            return 'dokumentasjon.adopsjon.vedleggtittel';
-        case Dokumentasjonsbehov.ANNEN_DOKUMENTASJON:
-            return 'dokumentasjon.annendokumentasjon.vedleggtittel';
-        case Dokumentasjonsbehov.AVTALE_DELT_BOSTED:
-            return 'dokumentasjon.deltbosted.vedleggtittel';
-        case Dokumentasjonsbehov.BEKREFTELSE_FRA_BARNEVERN:
-            return 'dokumentasjon.bekreftelsebarnevernet.vedleggtittel';
-        case Dokumentasjonsbehov.BOR_FAST_MED_SØKER:
-            return 'dokumentasjon.bekreftelseborsammen.vedleggtittel';
-        case Dokumentasjonsbehov.MEKLINGSATTEST:
-            return 'dokumentasjon.meklingsattest.vedleggtittel';
-        case Dokumentasjonsbehov.SEPARERT_SKILT_ENKE:
-            return 'dokumentasjon.separasjonskilsmissedødsfall.vedleggtittel';
-        case Dokumentasjonsbehov.VEDTAK_OPPHOLDSTILLATELSE:
-            return 'dokumentasjon.oppholdstillatelse.vedleggtittel';
-    }
-};
-
 export enum TittelSanityApiNavn {
     bekreftelsePaaAdopsjonTittel = 'bekreftelsePaaAdopsjonTittel',
     annenDokumentasjon = 'annenDokumentasjon',
