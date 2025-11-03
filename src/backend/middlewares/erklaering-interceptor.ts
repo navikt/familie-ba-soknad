3049c9b5a19952533160b0330fa6a77705774e7f
ole-kristian-rudjord <olekristianrudjord@gmail.com>
2025-11-03 12:00:41 +0100

    Endrer erklaering-interceptor til å være lik KS for å unngå 403 feil

--- src/backend/middlewares/erklaering-interceptor.ts
+++ src/backend/middlewares/erklaering-interceptor.ts
@@ -1,11 +1,7 @@
-import { Request, Response, NextFunction, RequestHandler } from 'express';
+import { RequestHandler, Request, Response, NextFunction } from 'express';
 
 import { byggFeiletRessurs } from '@navikt/familie-typer';
 
-import engelsk from '../../frontend/assets/lang/en.json' with { type: 'json' };
-import bokmål from '../../frontend/assets/lang/nb.json' with { type: 'json' };
-import nynorsk from '../../frontend/assets/lang/nn.json' with { type: 'json' };
-import { LocaleType } from '../../frontend/typer/common';
 import { ISøknadKontrakt } from '../../frontend/typer/kontrakt/kontrakt';
 
 export const hentSpråkteksterAlleSpråk = (språknøkkel: string): Record<LocaleType, string> => {
@@ -18,18 +14,14 @@ export const hentSpråkteksterAlleSpråk = (språknøkkel: string): Record<Local
 
 export const erklaeringInterceptor: RequestHandler = (request: Request, response: Response, next: NextFunction) => {
     const søknad: ISøknadKontrakt = request.body;
-    const spmKey = 'lestOgForståttBekreftelse';
-    const aksepterteSvarSpråkNøkkel = 'forside.bekreftelsesboks.erklæring.spm';
-    const aksepterteSvar = Object.values(hentSpråkteksterAlleSpråk(aksepterteSvarSpråkNøkkel));
+    const lestOgForståttErklæringKey = 'lestOgForståttBekreftelse';
 
-    if (!('spørsmål' in søknad && spmKey in søknad.spørsmål && 'verdi' in søknad.spørsmål[spmKey])) {
+    if (!(lestOgForståttErklæringKey in søknad)) {
         response.status(400).send(byggFeiletRessurs('Ugyldig søknadformat'));
         return;
     }
 
-    const svar = søknad.spørsmål[spmKey];
-
-    if (aksepterteSvar.includes(svar.verdi[søknad.originalSpråk])) {
+    if (søknad.lestOgForståttBekreftelse) {
         next();
     } else {
         response.status(403).send(byggFeiletRessurs('Du må huke av for at du oppgir korrekte opplysninger'));
