d9c6df790dd54016484966bbe3157a301c248a8b
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2025-11-11 22:06:19 +0100

    Fiks import av språk-filer til å samsvare med andre imports

--- src/frontend/utils/tekster.ts
+++ src/frontend/utils/tekster.ts
@@ -1,5 +1,5 @@
-import * as engelsk from '../assets/lang/en.json';
-import * as bokmål from '../assets/lang/nb.json';
-import * as nynorsk from '../assets/lang/nn.json';
+import engelsk from '../assets/lang/en.json' with { type: 'json' };
+import bokmål from '../assets/lang/nb.json' with { type: 'json' };
+import nynorsk from '../assets/lang/nn.json' with { type: 'json' };
 
 export const tekster = { nb: { ...bokmål }, nn: { ...nynorsk }, en: { ...engelsk } };
