1067f82611a6d464ad1a08e4af2e6852f4339b31
Ole Kristian Rudjord <89088327+ole-kristian-rudjord@users.noreply.github.com>
2025-12-17 09:30:12 +0100

    Endrer innhold på ModellVersjonModal (#1980)
    * Endrer ModellVersjonModal til å bruke Sanity tekster
    
    * Fjerner Alert med duplikat tekst og legger til error ikon i tittel

---
+++ src/frontend/typer/sanity/modaler/mistetInformasjonenDin.ts
@@ -0,0 +1,7 @@
+import { LocaleRecordBlock } from '../sanity';
+
+export interface IMistetInformasjonenDinModal {
+    tittel: LocaleRecordBlock;
+    info: LocaleRecordBlock;
+    knapp: LocaleRecordBlock;
+}
