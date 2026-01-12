ec0c9704e8677c0e940860122f1ac3c2422b433f
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2026-01-12 14:38:19 +0100

    Legg til to umami tracking events for: søknadtype og ytelseType

---
+++ src/frontend/umami.ts
@@ -0,0 +1,27 @@
+import { ESøknadstype } from '../common/typer/kontrakt/generelle';
+
+declare global {
+    interface Window {
+        umami: { track: (eventName: string, eventData?: string | object) => void };
+    }
+}
+
+function sendTilUmami(eventNavn: string, obj: object) {
+    if (typeof window !== 'undefined' && window.umami) {
+        window.umami.track(eventNavn, obj);
+    }
+}
+
+export function trackSøknadStartet(søknadstype: ESøknadstype): void {
+    sendTilUmami('søknad_startet', {
+        appNavn: 'familie-ba-soknad',
+        søknadstype: søknadstype,
+    });
+}
+
+export function trackYtelseTypeFastsatt(ytelseType: 'EØS' | 'NASJONAL'): void {
+    sendTilUmami('ytelse_type_fastsatt', {
+        appNavn: 'familie-ba-soknad',
+        ytelseType: ytelseType,
+    });
+}
