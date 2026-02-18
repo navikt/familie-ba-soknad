691df135c893784ce39a919738bdc66aff75ce2d
ole-kristian-rudjord <olekristianrudjord@gmail.com>
2026-02-18 15:47:05 +0100

    Endrer til Sanity tekster i LandDropdown.tsx

--- src/frontend/components/Felleskomponenter/Dropdowns/LandDropdown.tsx
+++ src/frontend/components/Felleskomponenter/Dropdowns/LandDropdown.tsx
@@ -1,10 +1,10 @@
 import React, { ReactNode } from 'react';
 
 import { Alpha3Code, getAlpha3Codes, getName } from 'i18n-iso-countries';
-import { useIntl } from 'react-intl';
 
 import type { Felt, ISkjema } from '@navikt/familie-skjema';
 
+import { useAppContext } from '../../../context/AppContext';
 import { useEøsContext } from '../../../context/EøsContext';
 import { useSpråkContext } from '../../../context/SpråkContext';
 import { SkjemaFeltTyper } from '../../../typer/skjema';
@@ -21,7 +21,7 @@ interface LandDropdownProps {
 }
 
 export const LandDropdown: React.FC<LandDropdownProps> = props => {
-    const intl = useIntl();
+    const { tekster, plainTekst } = useAppContext();
     const { valgtLocale } = useSpråkContext();
     const { erEøsLand } = useEøsContext();
     const kunEøs = props.kunEøs ?? false;
@@ -32,14 +32,15 @@ export const LandDropdown: React.FC<LandDropdownProps> = props => {
             const landB = getName(b, valgtLocale);
             if (landA && landB) {
                 return landA >= landB ? 1 : -1;
-            } else return 0;
+            }
+            return 0;
         })
         .filter(landKode => (kunEøs ? erEøsLand(landKode as Alpha3Code) : true))
         .filter(landKode => (props.ekskluderNorge ? landKode !== 'NOR' : true));
 
     return (
         <StyledDropdown<Alpha3Code | ''>
-            placeholder={intl.formatMessage({ id: 'felles.velg-land.placeholder' })}
+            placeholder={plainTekst(tekster().FELLES.hjelpeteksterForInput.velgLandPlaceholder)}
             {...props}
         >
             {landkoderSortertPåNavn.map(
