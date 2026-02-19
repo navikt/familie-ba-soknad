80c7d47e16492de9dc3ea76af6d056e43d50bee2
ole-kristian-rudjord <olekristianrudjord@gmail.com>
2026-02-19 11:41:43 +0100

    Fikser tester ved å bruke samme metode som i KS

--- src/frontend/components/Felleskomponenter/TilfeldigBarnIkon/TilfeldigBarnIkon.test.tsx
+++ src/frontend/components/Felleskomponenter/TilfeldigBarnIkon/TilfeldigBarnIkon.test.tsx
@@ -2,54 +2,45 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 import { vi } from 'vitest';
+import { mockDeep } from 'vitest-mock-extended';
 
+import { ISøknad } from '../../../typer/søknad';
 import * as hjelpefunksjoner from '../../../utils/hjelpefunksjoner';
-import { TestProvidereMedEkteTekster } from '../../../utils/testing';
+import { spyOnUseApp } from '../../../utils/testing';
 
 import { TilfeldigBarnIkon } from './TilfeldigBarnIkon';
 
 describe('TilfeldigBarnIkon', () => {
+    beforeEach(() => {
+        spyOnUseApp({});
+    });
+
+    afterEach(() => {
+        vi.restoreAllMocks();
+    });
+
     it('velger nytt ikon ved rerender by default', () => {
         const spy = vi.spyOn(hjelpefunksjoner, 'randomIntFraIntervall');
-        const { rerender } = render(
-            <TestProvidereMedEkteTekster>
-                <TilfeldigBarnIkon />
-            </TestProvidereMedEkteTekster>
-        );
+        const { rerender } = render(<TilfeldigBarnIkon />);
 
         // 2 ved første render, 1 for initiell useState, som ignoreres videre
         expect(spy.mock.calls.length).toEqual(2);
 
-        rerender(
-            <TestProvidereMedEkteTekster>
-                <TilfeldigBarnIkon />
-            </TestProvidereMedEkteTekster>
-        );
+        rerender(<TilfeldigBarnIkon />);
         expect(spy.mock.calls.length).toEqual(3);
 
-        rerender(
-            <TestProvidereMedEkteTekster>
-                <TilfeldigBarnIkon />
-            </TestProvidereMedEkteTekster>
-        );
+        rerender(<TilfeldigBarnIkon />);
         expect(spy.mock.calls.length).toEqual(4);
     });
 
     it('kan låse barnikon mellom rerenders med prop', () => {
         const spy = vi.spyOn(hjelpefunksjoner, 'randomIntFraIntervall');
+        spyOnUseApp(mockDeep<ISøknad>({ barnInkludertISøknaden: [{ id: '1' }, { id: '2' }] }));
 
-        const { rerender } = render(
-            <TestProvidereMedEkteTekster>
-                <TilfeldigBarnIkon byttVedRerender={false} />
-            </TestProvidereMedEkteTekster>
-        );
+        const { rerender } = render(<TilfeldigBarnIkon byttVedRerender={false} />);
         expect(spy.mock.calls.length).toEqual(1);
 
-        rerender(
-            <TestProvidereMedEkteTekster>
-                <TilfeldigBarnIkon byttVedRerender={false} />
-            </TestProvidereMedEkteTekster>
-        );
+        rerender(<TilfeldigBarnIkon byttVedRerender={false} />);
         expect(spy.mock.calls.length).toEqual(1);
     });
 });
