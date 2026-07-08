3d4e93bcfad0ec9883800b67a19a237a005708db
Stig Helge Strøm <stig.helge.strom@nav.no>
2026-07-08 13:45:13 +0200

    Lagt tilbake faro

--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -14,6 +14,7 @@ import { SpråkProvider } from './context/SpråkContext';
 import { hentDekorator } from './decorator';
 import MiljøProvider from './MiljøProvider';
 import { initApm } from './utils/apm';
+import { initGrafanaFaro } from './utils/grafanaFaro';
 
 import '@navikt/ds-css';
 
@@ -33,6 +34,7 @@ hentDekorator();
 
 polyfillLocaledata().then(async () => {
     initApm();
+    initGrafanaFaro();
 
     if (process.env.NODE_ENV !== 'production') {
         import('@axe-core/react').then(({ default: axe }) => {
